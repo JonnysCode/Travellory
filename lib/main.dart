d4a3dac5041785b73388f20804ccf54cf42bcd96
hessgia1 <hessgia1@students.zhaw.ch>
2020-03-06 12:17:52 +0100

    Use auth provider as wrapper

--- lib/main.dart
+++ lib/main.dart
@@ -5,25 +5,29 @@ import 'package:travellory/screens/wrapper.dart';
 import 'package:travellory/services/auth.dart';
 import 'package:travellory/providers/auth_provider.dart';
 
+import 'services/auth.dart';
+import 'services/auth.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return StreamProvider<User>.value(
-      value: AuthProvider(
-        auth: AuthService(),
-      ).auth.user,
-      child: MaterialApp(
-        title: 'Travellory',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primaryColor: Color(0xFFF72349),
-          accentColor: Color(0xFFEFC2D1),
-          scaffoldBackgroundColor: Color(0xFFF7E4E6),
+    return AuthProvider(
+      auth: AuthService(),
+      child: StreamProvider<User>.value(
+        value: AuthService().user,
+        child: MaterialApp(
+          title: 'Travellory',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            primaryColor: Color(0xFFF72349),
+            accentColor: Color(0xFFEFC2D1),
+            scaffoldBackgroundColor: Color(0xFFF7E4E6),
+          ),
+          home: Wrapper(),
         ),
-        home: Wrapper(),
       ),
     );
   }
