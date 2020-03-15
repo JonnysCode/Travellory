25648cefd847c3c1d638fb8c60afa82911de6147
Nadine Stadelmann <stadena1@students.zhaw.ch>
2020-03-15 19:12:06 +0100

    + UI implementation for booking

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:travellory/providers/auth_provider.dart';
 import 'package:travellory/services/auth.dart';
+import 'package:travellory/screens/trip/booking.dart';
 
 import '../../services/auth.dart';
 
@@ -13,20 +14,45 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Color color = Theme.of(context).primaryColor;
+
+    Widget buttonSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          IconButton(
+              icon: Icon(Icons.add),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Booking()),
+                );
+              }
+          ),
+          Text('Add Booking Information')
+        ],
+      ),
+    );
+
     return Scaffold(
-      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-      appBar: AppBar(
-        title: Text('travellory'),
-        backgroundColor: Theme.of(context).primaryColor,
-        elevation: 0.0,
-        actions: <Widget>[
-          FlatButton.icon(
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        appBar: AppBar(
+          title: Text('travellory'),
+          backgroundColor: Theme.of(context).primaryColor,
+          elevation: 0.0,
+          actions: <Widget>[
+            FlatButton.icon(
               onPressed: () => _signOut(context),
               icon: Icon(Icons.person),
               label: Text('logout'),
-          )
-        ],
-      ),
+            )
+          ],
+        ),
+        body: ListView(
+          children: [
+            buttonSection
+          ],
+        )
     );
   }
 }
