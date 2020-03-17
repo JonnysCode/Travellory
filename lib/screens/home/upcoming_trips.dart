9c0585a96cebffa121ba0c1300a751fce2f9b60e
grussjon <grussjon@students.zhaw.ch>
2020-03-17 08:43:21 +0100

    Bottom nav design updates

--- lib/screens/home/upcoming_trips.dart
+++ lib/screens/home/upcoming_trips.dart
@@ -8,7 +8,7 @@ class UpcomingTrips extends StatefulWidget {
 
 class _UpcomingTripsState extends State<UpcomingTrips> {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
-  static final _heightOfNavbar = 60;
+  static final _heightOfNavbar = 68;
   int _selectedIndex = 0;
   static const TextStyle optionStyle =
   TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Color(0xFF61BAA9)); // TODO: primary color declare as const
@@ -36,72 +36,85 @@ class _UpcomingTripsState extends State<UpcomingTrips> {
     return Scaffold(
       key: _scaffoldKey,
       backgroundColor: Theme.of(context).primaryColor,
-      body: Column(
-        children: <Widget>[
-          SizedBox(
-            height: (MediaQuery.of(context).size.height - _heightOfNavbar) * 0.05,
-          ),
-          Container(
-            child: DecoratedBox(
-              decoration: BoxDecoration(
-                color: Theme.of(context).canvasColor,
-              ),
-              child: ClipRRect(
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(40.0),
-                    topRight: Radius.circular(40.0)),
-                child: Container(
-                  child: Column(
-                    children: <Widget>[
-                    ],
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            SizedBox(
+              height: (MediaQuery.of(context).size.height - _heightOfNavbar) * 0.05,
+            ),
+            Container(
+              child: DecoratedBox(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).canvasColor,
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(40.0),
+                      topRight: Radius.circular(40.0)),
+                  child: Container(
+                    child: Column(
+                      children: <Widget>[
+                      ],
+                    ),
+                    color: Theme.of(context).scaffoldBackgroundColor,
+                    height: (MediaQuery.of(context).size.height - _heightOfNavbar) * 0.95,
+                    width: MediaQuery.of(context).size.width,
                   ),
-                  color: Theme.of(context).scaffoldBackgroundColor,
-                  height: (MediaQuery.of(context).size.height - _heightOfNavbar) * 0.95,
-                  width: MediaQuery.of(context).size.width,
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
-      bottomNavigationBar: Container(
-        decoration: BoxDecoration(color: Colors.white, boxShadow: [
-          BoxShadow(blurRadius: 20, color: Colors.black.withOpacity(.1))
-        ]),
-        child: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
-            child: GNav(
-                gap: 8,
-                activeColor: Colors.white,
-                iconSize: 24,
-                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                duration: Duration(milliseconds: 800),
-                tabBackgroundColor: Colors.grey[800],
-                tabs: [
-                  GButton(
-                    icon: Icons.home,
-                    text: 'Home',
-                  ),
-                  GButton(
-                    icon: Icons.calendar_today,
-                    text: 'Calendar',
-                  ),
-                  GButton(
-                    icon: Icons.map,
-                    text: 'Map',
-                  ),
-                  GButton(
-                    icon: Icons.person,
-                    text: 'Profile',
-                  ),
-                ],
-                selectedIndex: _selectedIndex,
-                onTabChange: (index) {
-                  setState(() {
-                    _selectedIndex = index;
-                  });
-                }),
+      bottomNavigationBar: DecoratedBox(
+        decoration: BoxDecoration(
+          color: Theme.of(context).scaffoldBackgroundColor,
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(40.0),
+              topRight: Radius.circular(40.0)),
+          child: Container(
+            decoration: BoxDecoration(color: Colors.white, boxShadow: [
+              BoxShadow(blurRadius: 20, color: Colors.black.withOpacity(.1))
+            ]),
+            child: SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 12),
+                child: GNav(
+                    gap: 8,
+                    activeColor: Colors.white,
+                    iconSize: 24,
+                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                    duration: Duration(milliseconds: 1000),
+                    tabBackgroundColor: Theme.of(context).primaryColor,
+                    color: Theme.of(context).primaryColor,
+                    tabs: [
+                      GButton(
+                        icon: Icons.home,
+                        text: 'Home',
+                      ),
+                      GButton(
+                        icon: Icons.calendar_today,
+                        text: 'Calendar',
+                      ),
+                      GButton(
+                        icon: Icons.map,
+                        text: 'Map',
+                      ),
+                      GButton(
+                        icon: Icons.person,
+                        text: 'Profile',
+                      ),
+                    ],
+                    selectedIndex: _selectedIndex,
+                    onTabChange: (index) {
+                      setState(() {
+                        _selectedIndex = index;
+                      });
+                    }),
+              ),
+            ),
           ),
         ),
       ),
