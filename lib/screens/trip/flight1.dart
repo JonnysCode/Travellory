ac30b98622e5868d9e86f7a6125494705c5b0b89
Nadine Stadelmann <stadena1@students.zhaw.ch>
2020-03-20 17:40:41 +0100

    + added backend implementation for flight

--- lib/screens/trip/flight1.dart
+++ lib/screens/trip/flight1.dart
@@ -1,6 +1,9 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:travellory/providers/auth_provider.dart';
 import 'package:travellory/services/auth.dart';
+import 'package:travellory/models/flightModel.dart';
+import 'package:cloud_functions/cloud_functions.dart';
 
 class Flight extends StatefulWidget {
   final Function toggleView;
@@ -12,11 +15,27 @@ class Flight extends StatefulWidget {
 }
 
 class _FlightState extends State<Flight> {
+  final TextEditingController _bookingReferenceController =
+      TextEditingController();
+  final TextEditingController _airlineController = TextEditingController();
+  final TextEditingController _flightNrController = TextEditingController();
+  final TextEditingController _seatController = TextEditingController();
+  final TextEditingController _departureLocationController =
+      TextEditingController();
+  final TextEditingController _departureTimeController =
+      TextEditingController();
+  final TextEditingController _arrivalLocationController =
+      TextEditingController();
+  final TextEditingController _arrivalTimeController = TextEditingController();
+  final TextEditingController _notesController = TextEditingController();
   bool _valueBaggage = false;
   bool _valueExcessBaggage = false;
 
-  void _valueBaggageChanged(bool value) => setState(() => _valueBaggage = value);
-  void _valueExcessBaggageChanged(bool value) => setState(() => _valueExcessBaggage = value);
+  void _valueBaggageChanged(bool value) =>
+      setState(() => _valueBaggage = value);
+
+  void _valueExcessBaggageChanged(bool value) =>
+      setState(() => _valueExcessBaggage = value);
 
   Future _signOut(BuildContext context) async {
     final BaseAuthService _auth = AuthProvider.of(context).auth;
@@ -47,6 +66,7 @@ class _FlightState extends State<Flight> {
           children: [
             SizedBox(height: 20.0),
             TextField(
+              controller: _bookingReferenceController,
               decoration: InputDecoration(
                 border: OutlineInputBorder(),
                 labelText: 'Booking Reference',
@@ -54,6 +74,7 @@ class _FlightState extends State<Flight> {
             ),
             SizedBox(height: 20.0),
             TextField(
+              controller: _airlineController,
               decoration: InputDecoration(
                 border: OutlineInputBorder(),
                 labelText: 'Airline',
@@ -61,6 +82,7 @@ class _FlightState extends State<Flight> {
             ),
             SizedBox(height: 20.0),
             TextField(
+              controller: _flightNrController,
               decoration: InputDecoration(
                 border: OutlineInputBorder(),
                 labelText: 'Flight Nr.',
@@ -68,6 +90,7 @@ class _FlightState extends State<Flight> {
             ),
             SizedBox(height: 20.0),
             TextField(
+              controller: _seatController,
               decoration: InputDecoration(
                 border: OutlineInputBorder(),
                 labelText: 'Seat',
@@ -90,6 +113,7 @@ class _FlightState extends State<Flight> {
             Row(children: [
               Expanded(
                 child: TextField(
+                  controller: _departureLocationController,
                   decoration: InputDecoration(hintText: "Location"),
                 ),
               ),
@@ -98,6 +122,7 @@ class _FlightState extends State<Flight> {
               ),
               Expanded(
                 child: TextField(
+                  controller: _departureTimeController,
                   decoration: InputDecoration(hintText: "Date / Time"),
                 ),
               ),
@@ -122,6 +147,7 @@ class _FlightState extends State<Flight> {
             Row(children: [
               Expanded(
                 child: TextField(
+                  controller: _arrivalLocationController,
                   decoration: InputDecoration(hintText: "Location"),
                 ),
               ),
@@ -130,6 +156,7 @@ class _FlightState extends State<Flight> {
               ),
               Expanded(
                 child: TextField(
+                  controller: _arrivalTimeController,
                   decoration: InputDecoration(hintText: "Date / Time"),
                 ),
               ),
@@ -147,25 +174,23 @@ class _FlightState extends State<Flight> {
       child: new Column(
         children: <Widget>[
           new CheckboxListTile(
-                value: _valueBaggage,
-                onChanged: _valueBaggageChanged,
-                title: new Text(
-                  'Checked baggage included in ticket?',
-                  style: TextStyle(fontSize: 16),
-                ),
-              controlAffinity: ListTileControlAffinity.leading
+              value: _valueBaggage,
+              onChanged: _valueBaggageChanged,
+              title: new Text(
+                'Checked baggage included in ticket?',
+                style: TextStyle(fontSize: 16),
               ),
+              controlAffinity: ListTileControlAffinity.leading),
           new CheckboxListTile(
               value: _valueExcessBaggage,
               onChanged: _valueExcessBaggageChanged,
               title: new Text(
                 'Excess baggage included in ticket?',
                 style: TextStyle(fontSize: 16),
-          ),
-              controlAffinity: ListTileControlAffinity.leading
-          ),
-            ],
-          ),
+              ),
+              controlAffinity: ListTileControlAffinity.leading),
+        ],
+      ),
     );
 
     Widget notes = Container(
@@ -176,10 +201,34 @@ class _FlightState extends State<Flight> {
             // TODO: adapt height
             SizedBox(height: 40.0),
             TextField(
+              controller: _notesController,
               decoration: InputDecoration(
                 border: OutlineInputBorder(),
                 labelText: 'Notes',
               ),
+            ),
+            RaisedButton(
+              key: Key('testFunctionCall'),
+              color: Theme.of(context).primaryColor,
+              child: Text(
+                'Add Flight',
+                style: TextStyle(color: Colors.white),
+              ),
+              onPressed: () async {
+                FlightModel flight = new FlightModel(
+                    bookingReference: _bookingReferenceController.text,
+                    airline: _airlineController.text,
+                    flightNr: _flightNrController.text,
+                    seat: _seatController.text,
+                    departureLocation: _departureLocationController.text,
+                    departureTime: _departureTimeController.text,
+                    arrivalLocation: _arrivalLocationController.text,
+                    arrivalTime: _arrivalTimeController.text,
+                    checkedBaggage: _valueBaggage,
+                    excessBaggage: _valueExcessBaggage,
+                    notes: _notesController.text);
+                _addFlight(flight);
+              },
             ),
           ],
         ),
@@ -203,5 +252,25 @@ class _FlightState extends State<Flight> {
         body: ListView(
           children: [buttonSection, flight, departure, arrival, baggage, notes],
         ));
+  }
+}
+
+void _addFlight(FlightModel flight) async {
+  //final CloudFunctions firebaseFunctions = new CloudFunctions(app: FirebaseApp.instance, region: 'asia-northeast1');
+  HttpsCallable callable =
+      CloudFunctions(app: FirebaseApp.instance, region: 'europe-west2')
+          .getHttpsCallable(functionName: 'payment-makePayment');
+  try {
+    final HttpsCallableResult result =
+        await callable.call(<String, dynamic>{"bookingReference": "123123"});
+    print(result.data);
+  } on CloudFunctionsException catch (e) {
+    print('caught firebase functions exception');
+    print(e.code);
+    print(e.message);
+    print(e.details);
+  } catch (e) {
+    print('caught generic exception');
+    print(e);
   }
 }
