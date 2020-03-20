f78fc7b725f75b43b14de09aa7de4f7bddcafc7c
Nadine Stadelmann <stadena1@students.zhaw.ch>
2020-03-20 18:59:06 +0100

    + updated flight

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -4,6 +4,7 @@ import 'package:travellory/screens/home/pages/calendar_page.dart';
 import 'package:travellory/screens/home/pages/home_page.dart';
 import 'package:travellory/screens/home/pages/map_page.dart';
 import 'package:travellory/screens/home/pages/profile_page.dart';
+import 'package:travellory/screens/home/pages/flight_page.dart';
 import 'package:travellory/widgets/layout.dart';
 
 class Home extends StatefulWidget {
@@ -22,89 +23,94 @@ class _HomeState extends State<Home> {
   int _prevSelectedIndex = 0;
   int _navBarIndex = 0;
 
-  List<Widget> _layoutPages(){
+  List<Widget> _layoutPages() {
     List<Widget> layoutPages = List();
     List<Widget> _pages = [
       HomePage(),
       CalendarPage(),
       MapPage(),
-      ProfilePage()
+      ProfilePage(),
+      FlightPage()
     ];
 
-    for(Widget page in _pages){
-      layoutPages.add(mainPageLayout(context, (MediaQuery.of(context).size.height - _heightOfNavBar), page));
+    for (Widget page in _pages) {
+      layoutPages.add(mainPageLayout(context,
+          (MediaQuery.of(context).size.height - _heightOfNavBar), page));
     }
     return layoutPages;
   }
 
-  Widget _navigationBar(){
-      return DecoratedBox(
-        key: Key('nav_bar'),
-        decoration: BoxDecoration(
-          color: Theme.of(context).scaffoldBackgroundColor,
-        ),
-        child: ClipRRect(
-          borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(40.0),
-              topRight: Radius.circular(40.0)),
-          child: Container(
-            decoration: BoxDecoration(color: Colors.white, boxShadow: [
-              BoxShadow(blurRadius: 30, color: Colors.black.withOpacity(.25))
-            ]),
-            child: SafeArea(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 12),
-                child: GNav(
-                    gap: 8,
-                    activeColor: Colors.white,
-                    iconSize: 24,
-                    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                    duration: Duration(milliseconds: 1000),
-                    tabBackgroundColor: Theme.of(context).primaryColor,
-                    color: Theme.of(context).primaryColor,
-                    tabs: [
-                      GButton(
-                        key: Key('nav_home_button'),
-                        icon: Icons.home,
-                        text: 'Home',
-                      ),
-                      GButton(
-                        key: Key('nav_calendar_button'),
-                        icon: Icons.calendar_today,
-                        text: 'Calendar',
-                      ),
-                      GButton(
-                        key: Key('nav_map_button'),
-                        icon: Icons.map,
-                        text: 'Map',
-                      ),
-                      GButton(
-                        key: Key('nav_profile_button'),
-                        icon: Icons.person,
-                        text: 'Profile',
-                      ),
-                    ],
-                    selectedIndex: _navBarIndex,
-                    onTabChange: (index) {
-                      setState(() {
-                        _navBarIndex = index;
-                      });
-                    }),
-              ),
+  Widget _navigationBar() {
+    return DecoratedBox(
+      key: Key('nav_bar'),
+      decoration: BoxDecoration(
+        color: Theme.of(context).scaffoldBackgroundColor,
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(40.0), topRight: Radius.circular(40.0)),
+        child: Container(
+          decoration: BoxDecoration(color: Colors.white, boxShadow: [
+            BoxShadow(blurRadius: 30, color: Colors.black.withOpacity(.25))
+          ]),
+          child: SafeArea(
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 15.0, vertical: 12),
+              child: GNav(
+                  gap: 8,
+                  activeColor: Colors.white,
+                  iconSize: 24,
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                  duration: Duration(milliseconds: 1000),
+                  tabBackgroundColor: Theme.of(context).primaryColor,
+                  color: Theme.of(context).primaryColor,
+                  tabs: [
+                    GButton(
+                      key: Key('nav_home_button'),
+                      icon: Icons.home,
+                      text: 'Home',
+
+                    ),
+                    GButton(
+                      key: Key('nav_calendar_button'),
+                      icon: Icons.calendar_today,
+                      text: 'Calendar',
+                    ),
+                    GButton(
+                      key: Key('nav_map_button'),
+                      icon: Icons.map,
+                      text: 'Map',
+                    ),
+                    GButton(
+                      key: Key('nav_profile_button'),
+                      icon: Icons.person,
+                      text: 'Profile',
+                    ),
+                    GButton(
+                      key: Key('nav_flight_button'),
+                      icon: Icons.airline_seat_flat,
+                      text: 'Flight',
+                    ),
+                  ],
+                  selectedIndex: _navBarIndex,
+                  onTabChange: (index) {
+                    setState(() {
+                      _navBarIndex = index;
+                    });
+                  }),
             ),
           ),
         ),
-      );
-    }
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    if(_pageController.hasClients){
-      _pageController.animateToPage(
-          _navBarIndex,
-          duration: Duration(milliseconds: 1000),
-          curve: Curves.ease
-      );
+    if (_pageController.hasClients) {
+      _pageController.animateToPage(_navBarIndex,
+          duration: Duration(milliseconds: 1000), curve: Curves.ease);
     }
 
     return Scaffold(
@@ -115,9 +121,9 @@ class _HomeState extends State<Home> {
         scrollDirection: Axis.horizontal,
         children: _layoutPages(),
         onPageChanged: (index) {
-          if(_navBarIndex > _prevSelectedIndex){
+          if (_navBarIndex > _prevSelectedIndex) {
             _prevSelectedIndex++;
-          } else if (_navBarIndex < _prevSelectedIndex){
+          } else if (_navBarIndex < _prevSelectedIndex) {
             _prevSelectedIndex--;
           } else {
             setState(() {
@@ -130,5 +136,4 @@ class _HomeState extends State<Home> {
       bottomNavigationBar: _navigationBar(),
     );
   }
-
 }
