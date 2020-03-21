e54205b0161d6ab4f66e92f7d2033b79352860a4
grussjon <jonathangruss@hotmail.com>
2020-03-21 11:55:54 +0100

    Trip container and assets

--- lib/screens/home/pages/home_page.dart
+++ lib/screens/home/pages/home_page.dart
@@ -10,6 +10,49 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Container(
       key: Key('home_page'),
+      child: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 80,
+          ),
+          Container(
+            height: 200,
+            width: MediaQuery.of(context).size.width - 20,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(40),
+              color: Colors.white,
+              boxShadow: [BoxShadow(blurRadius: 6, color: Colors.black.withOpacity(.15), offset: Offset(3.0, 3.0))],
+            ),
+            child: Column(
+              children: <Widget>[
+                Container(
+                  height: 80,
+                  width: MediaQuery.of(context).size.width - 20,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(40),
+                    color: Color(0xFFCCD7DD),
+                  ),
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        height: 80,
+                        width: 80,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: AssetImage("assets/images/home/trip/trip_01.png"),
+                            fit: BoxFit.fitWidth,
+                            alignment: Alignment.bottomCenter,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
