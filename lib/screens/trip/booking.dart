12158bee757a9fc3be237c3d1e446c53cac12221
antilyas <yasmine.antille@gmail.com>
2020-03-22 23:29:26 +0100

    * edited home page to add booking button, ! routing does not work, removed changes to rentalCar for this branch

--- lib/screens/trip/booking.dart
+++ lib/screens/trip/booking.dart
@@ -110,21 +110,30 @@ class _BookingState extends State<Booking> {
 
     return Scaffold(
         backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-        appBar: AppBar(
-          title: Text('travellory'),
-          backgroundColor: Theme.of(context).primaryColor,
-          elevation: 0.0,
-          actions: <Widget>[
-            FlatButton.icon(
-              onPressed: () => _signOut(context),
-              icon: Icon(Icons.person),
-              label: Text('logout'),
-            )
-          ],
-        ),
-        body: ListView(
-          children: [buttonSection, typeDropdown],
-        ));
+//        appBar: AppBar(
+//          title: Text('travellory'),
+//          backgroundColor: Theme.of(context).primaryColor,
+//          elevation: 0.0,
+//          actions: <Widget>[
+//            FlatButton.icon(
+//              onPressed: () => _signOut(context),
+//              icon: Icon(Icons.person),
+//              label: Text('logout'),
+//            )
+//          ],
+//        ),
+        body: Center(
+            child: RaisedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('Go back!'),
+            ),
+          ),
+//        ListView(
+//          children: [buttonSection, typeDropdown],
+//    )
+        );
   }
 
   void _testFunCall() async {
