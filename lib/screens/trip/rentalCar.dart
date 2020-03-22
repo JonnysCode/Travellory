4e781609f234b69b650b62e75f2298c084904d51
antilyas <antilyas@students.zhaw.ch>
2020-03-22 23:12:21 +0100

    + tried to add datepicker, has bug

--- lib/screens/trip/rentalCar.dart
+++ lib/screens/trip/rentalCar.dart
@@ -1,15 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:travellory/providers/auth_provider.dart';
-import 'package:travellory/services/auth.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_rounded_date_picker/rounded_picker.dart';
 
-class RentalCar extends StatelessWidget {
+class RentalCar extends StatefulWidget {
   final Function toggleView;
 
   RentalCar({this.toggleView});
 
-  Future _signOut(BuildContext context) async {
-    final BaseAuthService _auth = AuthProvider.of(context).auth;
-    await _auth.signOut();
+  @override
+  _RentalCarState createState() => _RentalCarState();
+}
+
+class _RentalCarState extends State<RentalCar> {
+  String selectedDate = '';
+  TextEditingController _date = new TextEditingController();
+
+  // this is the datepicker that will be called
+  // I'll look at the styling some more after it finally all works - Y
+  Future _selectDate(BuildContext context) async {
+    DateTime picked = await showRoundedDatePicker(
+      context: context,
+      theme: ThemeData.dark(),
+      initialDate: DateTime.now(),
+      firstDate: DateTime(DateTime.now().year - 1),
+      lastDate: DateTime(DateTime.now().year + 1),
+      borderRadius: 16,
+    );
+    if (picked != null) setState(() => selectedDate = picked.toString());
+    // or
+    // if (picked != null && picked != selectedDate) selectDate(picked);
   }
 
   @override
@@ -26,6 +45,61 @@ class RentalCar extends StatelessWidget {
             style: TextStyle(fontSize: 20),
           ),
         ],
+      ),
+    );
+
+    // ha usegfunde dass mit gesturedetector theoretisch das ghandlet söt werde für e textfield
+    // source: https://stackoverflow.com/questions/51907936/how-to-show-date-picker-on-the-onclick-of-text-field-instead-of-keyboard-in-flut
+    // example with light bulb: https://api.flutter.dev/flutter/widgets/GestureDetector-class.html
+    Widget datePicker = Container(
+      child: GestureDetector(
+        onTap: () => _selectDate(context),
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 0.0, horizontal: 50.0),
+                child: new Column(children: <Widget>[
+                  SizedBox(height: 20.0),
+                  TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      hintText: 'Date',
+                      labelText: "$selectedDate".split(' ')[0],
+                    ),
+                  ),
+                ]),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+
+    // here I tried to create the input box for the date similar to the we already had
+    // the weird thing is it only works with the box
+    // I don't want the datepicker to be opened when clicking the RaisedButton
+    // it should appear as soon as we press into the textfield
+    Widget date = Container(
+      padding: EdgeInsets.symmetric(vertical: 0.0, horizontal: 50.0),
+      child: new Center(
+        child: new Column(
+          children: <Widget>[
+            SizedBox(height: 20.0),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Date',
+                labelText: "$selectedDate".split(' ')[0],
+              ),
+            ),
+            new RaisedButton(
+              onPressed: () => _selectDate(context),
+              child: Text('Select date'),
+            ),
+          ],
+        ),
       ),
     );
 
@@ -83,20 +157,8 @@ class RentalCar extends StatelessWidget {
 
     return Scaffold(
         backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-        appBar: AppBar(
-          title: Text('travellory'),
-          backgroundColor: Theme.of(context).primaryColor,
-          elevation: 0.0,
-          actions: <Widget>[
-            FlatButton.icon(
-              onPressed: () => _signOut(context),
-              icon: Icon(Icons.person),
-              label: Text('logout'),
-            )
-          ],
-        ),
         body: ListView(
-          children: [buttonSection, rentalCar],
+          children: [buttonSection, datePicker, date, rentalCar],
         ));
   }
 }
