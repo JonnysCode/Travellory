cc2a211a7c02b6e9a8a866d28f1c6f7f8a46551e
antilyas <yasmine.antille@gmail.com>
2020-03-24 11:12:56 +0100

    - removed unnecessary import

--- lib/screens/trip/rentalCar.dart
+++ lib/screens/trip/rentalCar.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_rounded_date_picker/rounded_picker.dart';
 
 class RentalCar extends StatefulWidget {
