01ee070dd56d2ffea082e425b63ca99bacfa44ab
unknown <fluetfab@students.zhaw.ch>
2020-03-25 14:49:16 +0100

    add screen for changing password and picture

--- lib/screens/home/pages/profile_page.dart
+++ lib/screens/home/pages/profile_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:travellory/providers/auth_provider.dart';
 import 'package:travellory/services/auth.dart';
+import 'package:travellory/widgets/font_widgets.dart';
 
 class ProfilePage extends StatefulWidget {
   @override
@@ -24,6 +25,40 @@ class _ProfilePageState extends State<ProfilePage> {
             onPressed: () => _signOut(context),
             icon: Icon(Icons.person),
             label: Text('logout'),
+          ),
+          SizedBox(
+            height: 350,
+            width: 250,
+            child: const Card(
+              color: Colors.white60,
+                child: Text(
+                    'ProfileView\n',
+                    style: TextStyle(
+                    fontSize: 30,
+                    fontFamily: 'Arial',
+                    )
+                ),
+            ),
+          ),
+          RaisedButton(
+            onPressed: () {},
+            child: const Text(
+                'change password',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontFamily: 'Arial',
+            )
+            ),
+          ),
+          RaisedButton(
+            onPressed: () {},
+            child: const Text(
+                'change profile picture',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontFamily: 'Arial',
+                )
+            ),
           )
         ],
       ),
