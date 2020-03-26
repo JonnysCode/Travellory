52ebd99227d979732b2d28ff2f7dce04bfdc0150
antilyas <antilyas@students.zhaw.ch>
2020-03-26 17:31:34 +0100

    + added alertButton for dialog popups

--- lib/widgets/buttons.dart
+++ lib/widgets/buttons.dart
@@ -19,3 +19,22 @@ Widget filledButton(String text, Color splashColor, Color highlightColor,
     onPressed: () => function(),
   );
 }
+
+Widget alertButton(String text, BuildContext context, void function() ) {
+  return RaisedButton(
+    key: Key(text.toLowerCase().trim() + 'Button'),
+    highlightElevation: 0.0,
+    splashColor: Colors.white,
+    highlightColor: Theme.of(context).primaryColor,
+    elevation: 0.0,
+    color: Theme.of(context).hintColor,
+    shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30.0)),
+    child: Text(
+      text,
+      style: TextStyle(
+          fontWeight: FontWeight.bold, color: Colors.black54, fontSize: 20),
+    ),
+    onPressed: () => function(),
+  );
+}
