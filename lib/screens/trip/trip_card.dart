76713a9d6ee31f582acea29f067399a6dda3a816
grussjon <jonathangruss@hotmail.com>
2020-03-27 15:16:17 +0100

    UI refactoring trip card

--- lib/screens/trip/trip_card.dart
+++ lib/screens/trip/trip_card.dart
@@ -39,78 +39,81 @@ class _TripCardState extends State<TripCard> {
       ),
       child: Column(
         children: <Widget>[
-          Container(
-            height: 80,
-            width: MediaQuery.of(context).size.width - 20,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(40),
-              color: Color(0xFFCCD7DD),
-            ),
-            child: Row(
-              children: <Widget>[
-                Hero(
-                  tag: 'trip_image' + _tripModel.index.toString(),
-                  child: Container(
-                    height: 80,
-                    width: 80,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage(_tripModel.imagePath),
-                        fit: BoxFit.fitWidth,
-                        alignment: Alignment.bottomCenter,
+          GestureDetector(
+            onTap: () => _openTrip(),
+            child: Container(
+              height: 80,
+              width: MediaQuery.of(context).size.width - 20,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(40),
+                color: Color(0xFFCCD7DD),
+              ),
+              child: Row(
+                children: <Widget>[
+                  Hero(
+                    tag: 'trip_image' + _tripModel.index.toString(),
+                    child: Container(
+                      height: 80,
+                      width: 80,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage(_tripModel.imagePath),
+                          fit: BoxFit.fitWidth,
+                          alignment: Alignment.bottomCenter,
+                        ),
                       ),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(left: 8.0, top: 12.0, right: 30.0, bottom: 7.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      FashionFetishText(
-                        text: _tripModel.name,
-                        size: 22.0,
-                        fontWeight: FashionFontWeight.HEAVY,
-                      ),
-                      SizedBox(
-                        height: 13,
-                      ),
-                      FashionFetishText(
-                        text: _tripModel.startDate.toString().substring(0, 10)
-                          + ' - '
-                          + _tripModel.endDate.toString().substring(0, 10),
-                        size: 15.0,
-                        fontWeight: FashionFontWeight.BOLD,
-                        color: Colors.black54,
-                      ),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      Expanded(
-                        child: Row(
-                          children: <Widget>[
-                            Icon(
-                              Icons.location_on,
-                              size: 16,
-                              color: Colors.redAccent,
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(top: 6),
-                              child: FashionFetishText(
-                                text: _tripModel.destination,
-                                size: 14.0,
-                                fontWeight: FashionFontWeight.HEAVY,
-                                color: Colors.black54,
-                              ),
-                            ),
-                          ],
+                  Padding(
+                    padding: const EdgeInsets.only(left: 8.0, top: 12.0, right: 30.0, bottom: 7.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        FashionFetishText(
+                          text: _tripModel.name,
+                          size: 20.0,
+                          fontWeight: FashionFontWeight.HEAVY,
                         ),
-                      ),
-                    ],
-                  ),
-                )
-              ],
+                        SizedBox(
+                          height: 14,
+                        ),
+                        FashionFetishText(
+                          text: _tripModel.startDate.toString().substring(0, 10)
+                            + ' - '
+                            + _tripModel.endDate.toString().substring(0, 10),
+                          size: 14.0,
+                          fontWeight: FashionFontWeight.BOLD,
+                          color: Colors.black54,
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Expanded(
+                          child: Row(
+                            children: <Widget>[
+                              Icon(
+                                FontAwesomeIcons.locationArrow,
+                                size: 15,
+                                color: Colors.redAccent,
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(top: 6, left: 3),
+                                child: FashionFetishText(
+                                  text: _tripModel.destination,
+                                  size: 14.0,
+                                  fontWeight: FashionFontWeight.HEAVY,
+                                  color: Colors.black54,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
           Container(
