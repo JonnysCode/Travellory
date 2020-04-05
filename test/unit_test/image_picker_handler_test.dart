fd8fca21a4deef87cd582b0e6ed40b2960b79711
schinsev <schinsev@students.zhaw.ch>
2020-04-05 21:24:11 +0200

    delete test

--- test/unit_test/image_picker_handler_test.dart
+++ test/unit_test/image_picker_handler_test.dart
@@ -1,11 +1,6 @@
-import 'dart:io';
-
-import 'package:flutter/animation.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:image_cropper/image_cropper.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:travellory/utils/image_picker_handler.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
@@ -50,25 +45,4 @@ void main() {
       );
     });
   });
-
-  test('passes the image source argument correctly', () async {
-    AnimationController _controller;
-    ImagePickerListener _listener;
-    File file;
-    ImagePickerHandler handler = new ImagePickerHandler(_listener,_controller);
-
-    File croppedFile = await ImageCropper.cropImage(
-      //sourcePath: file.path,
-      aspectRatio: new CropAspectRatio(ratioX: 1.0, ratioY: 1.0),
-      maxWidth: 512,
-      maxHeight: 512,
-    );
-
-    expect(croppedFile, handler.cropImage(file));
-
-
-  });
-
-
-
 }
