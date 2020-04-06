0cd41a7d46228868404c182c8b713ef8ab643cb9
grussjon <grussjon@students.zhaw.ch>
2020-04-06 15:18:26 +0200

    Revert "* remove unexistend import"
    This reverts commit 9b0d33eae89c04d74511d4d50275d846b15bfca8.

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:travellory/screens/home/pages/calendar_page.dart';
 import 'package:travellory/screens/home/pages/home_page.dart';
 import 'package:travellory/screens/home/pages/map_page.dart';
