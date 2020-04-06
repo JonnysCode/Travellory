33fecb7b3dc98e7ca0a3103607728850aa1b6d5d
unknown <fluetfab@students.zhaw.ch>
2020-04-06 17:19:50 +0200

    add page for changing password

--- lib/screens/home/pages/profile_page.dart
+++ lib/screens/home/pages/profile_page.dart
@@ -15,6 +15,12 @@ class _ProfilePageState extends State<ProfilePage> {
     await Navigator.pushReplacementNamed(context, '/');
   }
 
+//  Future _changePassword(BuildContext context) async {
+//    //final BaseAuthService _auth = AuthProvider.of(context).auth;
+//    //await _auth.signOut();
+//    await Navigator.pushReplacementNamed(context, '/profile_page_passwod.dart');
+//  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -25,8 +31,78 @@ class _ProfilePageState extends State<ProfilePage> {
             onPressed: () => _signOut(context),
             icon: Icon(Icons.person),
             label: Text('logout'),
-          )
+          ),
+          SizedBox( //TODO: fluetfab remove if profile view is available
+            height: 350,
+            width: 250,
+            child: const Card(
+              color: Colors.white60,
+              child: Text(
+                  'ProfileView\n',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontFamily: 'Arial',
+                  )
+              ),
+            ),
+          ),
+          RaisedButton(
+            onPressed: () {
+              Navigator.of(context).push<void>(_createRoute());
+            }, //TODO: link new site profile_page_password
+            child: const Text(
+                'change password',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontFamily: 'Arial',
+                )
+            ),
+          ),
+          RaisedButton( //TODO: fluetfab remove if logout above profile view works
+            onPressed: () => _signOut(context),
+            child: const Text(
+                'Logout',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontFamily: 'Arial',
+                )
+            ),
+          ),
+
         ],
+      ),
+    );
+  }
+}
+
+Route _createRoute() {
+  return PageRouteBuilder<SlideTransition>(
+    pageBuilder: (context, animation, secondaryAnimation) => _Page2(),
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      var tween = Tween<Offset>(begin: Offset(0.0, 1.0), end: Offset.zero);
+      var curveTween = CurveTween(curve: Curves.ease);
+
+      return SlideTransition(
+        position: animation.drive(curveTween).drive(tween),
+        child: child,
+      );
+    },
+  );
+}
+
+class _Page2 extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: TextFormField(
+          decoration: InputDecoration(
+            labelStyle: TextStyle(color: Colors.black,fontSize: 20.0),
+            border: InputBorder.none,
+            hintText: '*********',
+            hintStyle: TextStyle(color: Colors.black),
+          ),
+        ),
       ),
     );
   }
