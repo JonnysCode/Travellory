ada3fa9362818ae1b5e216dd830e195ab8dba18b
grussjon <grussjon@students.zhaw.ch>
2020-04-07 07:19:45 +0200

    - Booking abstract replaced by Model

--- lib/models/day_model.dart
+++ lib/models/day_model.dart
@@ -1,17 +1,14 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:travellory/utils/date_converter.dart';
 
-import 'booking_model.dart';
 
 class Day {
   Day({
     @required this.date,
-    this.bookings,
   }){
     dateString = DateConverter.toDateStringFrom(date);
   }
 
   DateTime date;
   String dateString;
-  List<Booking> bookings;
 }
