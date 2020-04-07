5c17c88f1998f90999a18dce76d08b041d522b21
antilyas <antilyas@students.zhaw.ch>
2020-04-07 16:08:54 +0200

    * refactoring of booking header to reduce code duplicity

--- lib/screens/trip/bookings/activity.dart
+++ lib/screens/trip/bookings/activity.dart
@@ -3,15 +3,15 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:travellory/models/activity_model.dart';
 import 'package:travellory/models/trip_model.dart';
 import 'package:travellory/services/add_database.dart';
-import 'package:travellory/utils/date_converter.dart';
 import 'package:travellory/widgets/buttons.dart';
 import 'package:travellory/widgets/dropdown.dart';
-import 'package:travellory/widgets/font_widgets.dart';
 import 'package:travellory/widgets/form_field.dart';
 import 'package:travellory/widgets/section_titles.dart';
 import 'package:travellory/widgets/show_dialog.dart';
 import 'package:travellory/widgets/date_form_field.dart';
 import 'package:travellory/widgets/time_form_field.dart';
+
+import 'header.dart';
 
 class Activity extends StatefulWidget {
   @override
@@ -34,19 +34,19 @@ class _ActivityState extends State<Activity> {
       "You can see all the information in the trip overview";
 
   List<Item> types = <Item>[
-    const Item('Historic', Icon(FontAwesomeIcons.landmark, color: const Color(0xFF167F67))),
-    const Item('Outdoors', Icon(FontAwesomeIcons.mountain, color: const Color(0xFF167F67))),
-    const Item('Culture', Icon(FontAwesomeIcons.diagnoses, color: const Color(0xFF167F67))),
-    const Item('Social', Icon(FontAwesomeIcons.users, color: const Color(0xFF167F67))),
-    const Item('Relaxing', Icon(FontAwesomeIcons.hotTub, color: const Color(0xFF167F67))),
-    const Item('Adventure', Icon(FontAwesomeIcons.hiking, color: const Color(0xFF167F67))),
-    const Item('Dining', Icon(FontAwesomeIcons.utensils, color: const Color(0xFF167F67))),
-    const Item('Other', Icon(FontAwesomeIcons.futbol, color: const Color(0xFF167F67))),
+    const Item('Historic', Icon(FontAwesomeIcons.landmark, color: Color(0xFF167F67))),
+    const Item('Outdoors', Icon(FontAwesomeIcons.mountain, color: Color(0xFF167F67))),
+    const Item('Culture', Icon(FontAwesomeIcons.diagnoses, color: Color(0xFF167F67))),
+    const Item('Social', Icon(FontAwesomeIcons.users, color: Color(0xFF167F67))),
+    const Item('Relaxing', Icon(FontAwesomeIcons.hotTub, color: Color(0xFF167F67))),
+    const Item('Adventure', Icon(FontAwesomeIcons.hiking, color: Color(0xFF167F67))),
+    const Item('Dining', Icon(FontAwesomeIcons.utensils, color: Color(0xFF167F67))),
+    const Item('Other', Icon(FontAwesomeIcons.futbol, color: Color(0xFF167F67))),
   ];
 
   @override
   Widget build(BuildContext context) {
-    final TripModel _tripModel = ModalRoute.of(context).settings.arguments;
+    final TripModel tripModel = ModalRoute.of(context).settings.arguments;
 
     void returnToTripScreen() {
       Navigator.pop(context);
@@ -59,101 +59,7 @@ class _ActivityState extends State<Activity> {
         color: Colors.white,
         child: Column(
           children: <Widget>[
-            Container(
-              height: 190,
-              width: MediaQuery.of(context).size.width,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.only(bottomLeft: Radius.circular(80)),
-                color: Color(0xFFCCD7DD),
-              ),
-              child: Stack(
-                children: <Widget>[
-                  Positioned(
-                    top: 0,
-                    right: 0,
-                    child: IconButton(
-                      onPressed: () => returnToTripScreen(),
-                      icon: FaIcon(FontAwesomeIcons.times),
-                      iconSize: 26,
-                      color: Colors.red,
-                    ),
-                  ),
-                  Positioned(
-                    top: -30,
-                    left: -40,
-                    child: Hero(
-                      tag: 'trip_image${_tripModel.index.toString()}',
-                      child: Container(
-                        height: 220,
-                        width: 220,
-                        decoration: BoxDecoration(
-                          image: DecorationImage(
-                            image: AssetImage(_tripModel.imagePath),
-                            fit: BoxFit.fitWidth,
-                            alignment: Alignment.bottomCenter,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    left: 180,
-                    child: Container(
-                      padding: EdgeInsets.only(top: 40, left: 10, right: 10),
-                      alignment: Alignment.topLeft,
-                      width: MediaQuery.of(context).size.width,
-                      constraints: BoxConstraints(
-                          maxHeight: 100.0, maxWidth: MediaQuery.of(context).size.width - 200),
-                      child: FashionFetishText(
-                        text: _tripModel.name,
-                        size: 24,
-                        fontWeight: FashionFontWeight.HEAVY,
-                        height: 1.05,
-                      ),
-                    ),
-                  ),
-                  Positioned(
-                    top: 110,
-                    left: 190,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        FashionFetishText(
-                            text: 'From: ${DateConverter.format(_tripModel.startDate)}' +
-                                '\n' +
-                                'To: ${DateConverter.format(_tripModel.endDate)}',
-                            color: Colors.black54,
-                            fontWeight: FashionFontWeight.BOLD,
-                            size: 14,
-                            height: 1.25),
-                        SizedBox(
-                          height: 12,
-                        ),
-                        Row(
-                          children: <Widget>[
-                            Icon(
-                              FontAwesomeIcons.locationArrow,
-                              size: 15,
-                              color: Colors.redAccent,
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.only(top: 6, left: 3),
-                              child: FashionFetishText(
-                                text: _tripModel.destination,
-                                size: 14,
-                                fontWeight: FashionFontWeight.HEAVY,
-                                color: Colors.black54,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
+            getBookingHeader(context, tripModel),
             Expanded(
               //child: Form(
               child: SingleChildScrollView(
