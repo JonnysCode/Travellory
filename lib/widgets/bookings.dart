0b531c371300757216d1e4b01662ed3b9ad3be11
antilyas <yasmine.antille@gmail.com>
2020-04-08 10:56:37 +0200

    * updated submit trip

--- lib/widgets/bookings.dart
+++ lib/widgets/bookings.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:travellory/models/abstract_model.dart';
+import 'package:travellory/models/trip_model.dart';
 import 'package:travellory/services/add_database.dart';
 import 'package:travellory/widgets/forms/show_dialog.dart';
 
@@ -8,6 +9,17 @@ void Function() onSubmitBooking(Model model, String functionName, BuildContext c
     bool added = await DatabaseAdder.addModel(model, functionName);
     if (added) {
       showSubmittedBookingDialog(context, alertText);
+    } else {
+      // TODO(antilyas): add error message to user
+    }
+  };
+}
+
+void Function() onSubmitTrip(TripModel tripModel, String functionName, BuildContext context, alertText) {
+  return () async {
+    bool added = await DatabaseAdder.addModel(tripModel, functionName);
+    if (added) {
+      showSubmittedTripDialog(context, alertText);
     } else {
       // TODO(antilyas): add error message to user
     }
