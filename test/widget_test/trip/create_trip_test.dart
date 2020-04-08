19d1b2b73a8bdad4b017f94afd4b8967ab41bd2e
grussjon <jonathangruss@hotmail.com>
2020-04-08 22:35:32 +0200

    * Create trip tests

--- test/widget_test/trip/create_trip_test.dart
+++ test/widget_test/trip/create_trip_test.dart
@@ -1,105 +1,63 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:travellory/providers/auth_provider.dart';
 import 'package:travellory/screens/trip/create_trip_screen.dart';
+import 'package:travellory/services/auth.dart';
+import 'package:mockito/mockito.dart';
 
-class Wrapper extends StatelessWidget {
-  const Wrapper({ Key key }) : super(key: key);
-  @override
-  Widget build(BuildContext context) {
-    TripModel tripModel = TripModel(
-        name: 'Castle Discovery',
-        startDate: '2020-05-12',
-        endDate: '2020-05-25',
-        destination: 'Munich',
-        imageNr: 3
-    );
-    return GestureDetector(
-      onTap: () {
-        Navigator.pushNamed(context, '/booking/rentalCar', arguments: tripModel);
-      },
-      child: Container(
-        color: const Color(0xFFFFFF00),
-        child: const Text('X'),
-      ),
-    );
-  }
-}
+class MockAuth extends Mock implements BaseAuthService {}
 
 void main() {
-  Widget makeTestableWidget(){
-    return MaterialApp(
-      routes: <String, WidgetBuilder>{
-        '/': (context) => const Wrapper(),
-        '/booking/rentalCar': (context) => CreateTrip()
-      },
-    );
+  Widget makeTestableWidget({Widget child, BaseAuthService auth}) {
+    return AuthProvider(
+        auth: auth,
+        child: MaterialApp(
+          home: child,
+        ));
   }
 
-  Future<void> pumpRentalCar(WidgetTester tester) async {
-    await tester.tap(find.text('X'));
-    await tester.pump();
-  }
+  testWidgets('test if create trip page is loaded', (WidgetTester tester) async {
+    final testKey = Key('create_trip');
 
-  testWidgets('test if Rental Car page is loaded', (WidgetTester tester) async {
-    final testKey = Key('Rental Car');
+    MockAuth mockAuth = MockAuth();
+    Widget page = CreateTrip();
 
-    await tester.pumpWidget(makeTestableWidget());
+    await tester.pumpWidget(makeTestableWidget(child: page, auth: mockAuth));
 
-    expect(find.text('X'), findsOneWidget);
-    expect(find.byKey(testKey, skipOffstage: false), findsNothing);
-
-    await pumpRentalCar(tester);
-    expect(find.text('X'), findsOneWidget);
-    expect(find.byKey(testKey, skipOffstage: false), isOffstage);
+    expect(find.byKey(testKey), findsOneWidget);
   });
 
   testWidgets('test if form instance is found', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(makeTestableWidget());
+    Widget page = CreateTrip();
+    MockAuth mockAuth = MockAuth();
 
-    await pumpRentalCar(tester);
+    await tester.pumpWidget(makeTestableWidget(child: page, auth: mockAuth));
+
     // verify that form is present
-    expect(find.byType(Form, skipOffstage: false), isOffstage);
-  });
-
-  testWidgets('test if form is present', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(makeTestableWidget());
-    await pumpRentalCar(tester);
-
-    // Verify that form is present.
-    expect(find.byType(Form, skipOffstage: false), isOffstage);
+    expect(find.byType(Form), findsOneWidget);
   });
 
   testWidgets('test if all form fields are present', (WidgetTester tester) async {
-    await tester.pumpWidget(makeTestableWidget());
-    await pumpRentalCar(tester);
+    Widget page = CreateTrip();
+    MockAuth mockAuth = MockAuth();
 
-    expect(find.byKey(Key('BookingSiteTitle'), skipOffstage: false), findsOneWidget);
-    expect(find.byKey(Key('SectionTitle'), skipOffstage: false), findsNWidgets(5));
-    expect(find.byIcon(Icons.confirmation_number, skipOffstage: false), findsOneWidget);
-    expect(find.byIcon(Icons.supervised_user_circle, skipOffstage: false), findsOneWidget);
-    expect(find.byIcon(Icons.location_on, skipOffstage: false), findsNWidgets(2));
-    expect(find.byIcon(Icons.date_range, skipOffstage: false), findsNWidgets(2));
-    expect(find.byIcon(Icons.access_time, skipOffstage: false), findsNWidgets(2));
-    expect(find.byIcon(Icons.directions_car, skipOffstage: false), findsNWidgets(2));
-    expect(find.byIcon(Icons.speaker_notes, skipOffstage: false), findsOneWidget);
+    await tester.pumpWidget(makeTestableWidget(child: page, auth: mockAuth));
+
+    expect(find.text('Trip Details'), findsOneWidget);
+    expect(find.text('General Information'), findsOneWidget);
+    expect(find.text('End Date *'), findsOneWidget);
+    expect(find.text('Destination(s) *'), findsOneWidget);
+    expect(find.text('Trip Title *'), findsOneWidget);
+    expect(find.text('General Information'), findsOneWidget);
   });
 
-  testWidgets('test if submit button is present', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(makeTestableWidget());
-    await pumpRentalCar(tester);
-    // Verify that form is present.
-    expect(find.byKey(Key('SubmitButton'), skipOffstage: false), isOffstage);
+  testWidgets('test if all form fields are present', (WidgetTester tester) async {
+    Widget page = CreateTrip();
+    MockAuth mockAuth = MockAuth();
+
+    await tester.pumpWidget(makeTestableWidget(child: page, auth: mockAuth));
+
+    expect(find.byKey(Key('image_icon')), findsNWidgets(10));
   });
 
-  testWidgets('test if cancel button is present', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(makeTestableWidget());
-    await pumpRentalCar(tester);
-
-    // Verify that form is present.
-    expect(find.byKey(Key('CancelButton'), skipOffstage: false), isOffstage);
-  });
 }
