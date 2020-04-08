d3a4d1bfd97eb8b89d3fa363bf0abfc6576f7985
hessgia1 <hessgia1@students.zhaw.ch>
2020-04-09 01:10:47 +0200

    Refactor remove code smells

--- lib/services/user_management.dart
+++ lib/services/user_management.dart
@@ -2,7 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_functions/cloud_functions.dart';
 
 class UserManagement {
-  static setUsername(FirebaseUser user) {
+  static void setUsername(FirebaseUser user) {
     final HttpsCallable callable = CloudFunctions.instance.getHttpsCallable(
       functionName: 'user-setUsername',
     );
@@ -13,7 +13,7 @@ class UserManagement {
     final HttpsCallable callable = CloudFunctions.instance.getHttpsCallable(
       functionName: 'user-isUsernameAvailable',
     );
-    dynamic result = await callable.call({'displayName': '$username'});
+    final result = await callable.call({'displayName': '$username'});
     return result.data['isAvailable'];
   }
 }
