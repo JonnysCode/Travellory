41dc128ea867bc3f1b75f01ddb8c2b9101d5dcde
hessgia1 <hessgia1@students.zhaw.ch>
2020-04-09 16:12:03 +0200

    Fix merge

--- lib/services/user_management.dart
+++ lib/services/user_management.dart
@@ -13,11 +13,7 @@ class UserManagement {
     final HttpsCallable callable = CloudFunctions.instance.getHttpsCallable(
       functionName: 'user-isUsernameAvailable',
     );
-<<<<<<< HEAD
     final dynamic result = await callable.call({'displayName': '$username'});
-=======
-    final result = await callable.call({'displayName': '$username'});
->>>>>>> d3a4d1b... Refactor remove code smells
     return result.data['isAvailable'];
   }
 }
