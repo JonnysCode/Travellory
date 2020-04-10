161b22963634fb56f4a5ebf8e0b737e0082ba2cc
Nadine Stadelmann <stadena1@students.zhaw.ch>
2020-04-10 17:44:25 +0200

    + added functions to addTrip to DB and getTrips from the DB

--- lib/models/trip_model.dart
+++ lib/models/trip_model.dart
@@ -1,15 +1,14 @@
 import 'abstract_model.dart';
 
 class TripModel extends Model {
-  TripModel({
-    this.userUID,
-    this.name,
-    this.startDate,
-    this.endDate,
-    this.destination,
-    this.imageNr,
-    this.index
-  }){
+  TripModel(
+      {this.userUID,
+      this.name,
+      this.startDate,
+      this.endDate,
+      this.destination,
+      this.imageNr,
+      this.index}) {
     imagePath = 'assets/images/home/trip/trip_${imageNr.toString()}.png';
   }
 
@@ -23,40 +22,26 @@ class TripModel extends Model {
   int index;
 
   @override
-  Map<String, dynamic> toMap(){
+  Map<String, dynamic> toMap() {
     return {
-      'userUID': userUID,
-      'name': name,
-      'startDate': startDate,
-      'endDate': endDate,
-      'destination': destination,
-      'imagePath': imagePath,
-      'imageNr': imageNr,
-      'index': index,
+        "userUID": userUID,
+        "name": name,
+        "startDate": startDate,
+        "endDate": endDate,
+        "destination": destination,
+        "imageNr": imageNr
     };
+  }
+
+  TripModel.fromData(trip) {
+    this.userUID = trip["userUID"];
+    this.name = trip["name"];
+    this.startDate = trip["startDate"];
+    this.endDate = trip["endDate"];
+    this.destination = trip["destination"];
+    this.imageNr = trip["imageNr"];
+    this.imagePath = 'assets/images/home/trip/trip_${imageNr.toString()}.png';
   }
 }
 
-List<TripModel> tripModels = <TripModel>[
-  TripModel(
-    name: 'Castle Discovery',
-    startDate: '2020-06-23',
-    endDate: '2020-07-12',
-    destination: 'Munich',
-    imageNr: 3
-  ),
-  TripModel(
-      name: 'Beach Relaxation',
-      startDate: '2020-05-12',
-      endDate: '2020-05-18',
-      destination: 'Maledives',
-      imageNr: 1
-  ),
-  TripModel(
-      name: 'City Trip',
-      startDate: '2020-08-09',
-      endDate: '2020-08-21',
-      destination: 'San Francisco',
-      imageNr: 2
-  ),
-];
+List<TripModel> tripModels = <TripModel>[];
