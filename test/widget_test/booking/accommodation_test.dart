81877457f875936fe288cddc5a8fc3d959b0719d
antilyas <yasmine.antille@gmail.com>
2020-04-11 16:27:02 +0200

    * moved booking into own directory in screens

--- test/widget_test/booking/accommodation_test.dart
+++ test/widget_test/booking/accommodation_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:travellory/models/trip_model.dart';
-import 'package:travellory/screens/trip/bookings/accommodation.dart';
+import 'package:travellory/screens/bookings/add_accommodation.dart';
 
 class Wrapper extends StatelessWidget {
   const Wrapper({Key key}) : super(key: key);
