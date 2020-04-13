e2d5563a1f54c79f809c78790d32972a8091aefe
antilyas <yasmine.antille@gmail.com>
2020-04-13 15:45:47 +0200

    + added accommodation view

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
-import 'package:travellory/screens/bookings/view_flight.dart';
-import 'package:travellory/screens/bookings/view_rental_car.dart';
+import 'package:travellory/screens/bookings/view_accommodation.dart';
 import 'package:travellory/screens/home/pages/calendar_page.dart';
 import 'package:travellory/screens/home/pages/home_page.dart';
 import 'package:travellory/screens/home/pages/map_page.dart';
@@ -22,7 +21,7 @@ class _HomeState extends State<Home> {
     CalendarPage(),
     MapPage(),
     ProfilePage(),
-    FlightView(),
+    AccommodationView(),
   ];
 
   PageController _pageController;
@@ -134,8 +133,8 @@ class _HomeState extends State<Home> {
                       ),
                       GButton(
                         key: Key('nav_profile_button'),
-                        icon: FontAwesomeIcons.plane,
-                        text: 'Plane',
+                        icon: FontAwesomeIcons.hotel,
+                        text: 'Hotel',
                       ),
                     ],
                     selectedIndex: _navBarIndex,
