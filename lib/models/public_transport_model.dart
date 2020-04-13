33274340ce2714c3b1ba5531648f2822cccca279
antilyas <yasmine.antille@gmail.com>
2020-04-13 15:53:59 +0200

    + added public transport model

--- lib/models/public_transport_model.dart
+++ lib/models/public_transport_model.dart
@@ -1,6 +1,24 @@
 import 'package:travellory/models/abstract_model.dart';
 
 class PublicTransportModel extends Model {
+  PublicTransportModel({
+    this.transportationType,
+    this.company,
+    this.specificType,
+    this.booked,
+    this.seatReservation,
+    this.reference,
+    this.companyReservation,
+    this.seat,
+    this.departureLocation,
+    this.departureDate,
+    this.departureTime,
+    this.arrivalLocation,
+    this.arrivalDate,
+    this.arrivalTime,
+    this.notes,
+  });
+
   String transportationType;
   String company;
   String specificType;
@@ -18,7 +36,7 @@ class PublicTransportModel extends Model {
   String notes;
 
   @override
-  Map<String, dynamic> toMap(){
+  Map<String, dynamic> toMap() {
     return {
       'transportationType': transportationType,
       'company': company,
@@ -38,3 +56,24 @@ class PublicTransportModel extends Model {
     };
   }
 }
+
+List<PublicTransportModel> publicTransportModels = <PublicTransportModel> [
+  PublicTransportModel(
+    transportationType: 'Rail',
+    company: 'RENFE',
+    specificType: null,
+    booked: true,
+    seatReservation: true,
+    reference: 'R1A6',
+    companyReservation: 'RENFE',
+    seat: '13F',
+    departureLocation: 'Madrid',
+    departureDate: '2020-06-20',
+    departureTime: '12:00:00',
+    arrivalLocation: 'Sevilla',
+    arrivalDate: '2020-06-20',
+    arrivalTime: '15:23:00',
+    notes: 'Coche 8',
+  )
+];
+
