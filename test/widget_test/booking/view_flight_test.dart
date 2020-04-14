adc75f6d0a808a16814c7c9b834f6b894f5e1ec5
antilyas <yasmine.antille@gmail.com>
2020-04-14 21:54:55 +0200

    * renamed pump methods

--- test/widget_test/booking/view_flight_test.dart
+++ test/widget_test/booking/view_flight_test.dart
@@ -30,7 +30,7 @@ void main() {
     );
   }
 
-  Future<void> pumpFlight(WidgetTester tester) async {
+  Future<void> pumpFlightView(WidgetTester tester) async {
     await tester.tap(find.text('X'));
     await tester.pump();
   }
@@ -43,35 +43,35 @@ void main() {
     expect(find.text('X'), findsOneWidget);
     expect(find.byKey(testKey, skipOffstage: false), findsNothing);
 
-    await pumpFlight(tester);
+    await pumpFlightView(tester);
     expect(find.text('X'), findsOneWidget);
     expect(find.byKey(testKey, skipOffstage: false), isOffstage);
   });
 
   testWidgets('test if view booking header is present', (WidgetTester tester) async {
     await tester.pumpWidget(makeTestableWidget());
-    await pumpFlight(tester);
+    await pumpFlightView(tester);
 
     expect(find.byKey(Key('ViewBookingHeader'), skipOffstage: false), findsOneWidget);
   });
 
   testWidgets('test if view flight page content is present', (WidgetTester tester) async {
     await tester.pumpWidget(makeTestableWidget());
-    await pumpFlight(tester);
+    await pumpFlightView(tester);
 
     expect(find.byKey(Key('FlightViewPage'), skipOffstage: false), findsOneWidget);
   });
 
   testWidgets('test if bottom bar is present', (WidgetTester tester) async {
     await tester.pumpWidget(makeTestableWidget());
-    await pumpFlight(tester);
+    await pumpFlightView(tester);
 
     expect(find.byKey(Key('BottomBar'), skipOffstage: false), findsOneWidget);
   });
 
   testWidgets('test if all view fields are present', (WidgetTester tester) async {
     await tester.pumpWidget(makeTestableWidget());
-    await pumpFlight(tester);
+    await pumpFlightView(tester);
 
     expect(find.byKey(Key('SectionTitle'), skipOffstage: false), findsNWidgets(5));
     expect(find.byIcon(FontAwesomeIcons.ticketAlt, skipOffstage: false), findsNWidgets(2));
