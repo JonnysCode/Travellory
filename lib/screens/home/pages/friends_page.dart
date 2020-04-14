fe65f89ae62952bedc5df2607a64335f123e27ea
unknown <fluetfab@students.zhaw.ch>
2020-04-14 13:24:24 +0200

    remove the duplicate friends-page

--- lib/screens/home/pages/friends_page.dart
+++ lib/screens/home/pages/friends_page.dart
@@ -9,8 +9,40 @@ class _FriendsPageState extends State<FriendsPage> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Container(
-
+      child: Stack(
+        children: <Widget>[
+          SizedBox.expand(
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(6, 40, 6, 0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.vertical(top: Radius.circular(40.0)),
+                  boxShadow: [BoxShadow(blurRadius: 20, color: Colors.black.withOpacity(.2), offset: Offset(0.0, -6.0))],
+                ),
+                child: Column(
+                  key: Key('friends_page'),
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    SizedBox(height: 20),
+                    Container(
+                      child: Text(
+                        'FRIENDS PAGE',
+                        textAlign: TextAlign.center,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          fontSize: 26,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
