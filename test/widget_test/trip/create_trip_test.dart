c865d63c2adbdcb9644dd34f8829e46325394dee
grussjon <grussjon@students.zhaw.ch>
2020-04-15 19:57:57 +0200

    * Testing StreamProvider

--- test/widget_test/trip/create_trip_test.dart
+++ test/widget_test/trip/create_trip_test.dart
@@ -1,37 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:travellory/providers/auth_provider.dart';
 import 'package:travellory/screens/trip/create_trip_screen.dart';
-import 'package:travellory/services/auth.dart';
 import 'package:mockito/mockito.dart';
+import 'package:provider/provider.dart';
+import 'package:travellory/models/user_model.dart';
 
-class MockAuth extends Mock implements BaseAuthService {}
+class MockUser extends Mock implements UserModel {}
 
 void main() {
-  Widget makeTestableWidget({Widget child, BaseAuthService auth}) {
-    return AuthProvider(
-        auth: auth,
-        child: MaterialApp(
-          home: child,
-        ));
+  Widget makeTestableWidget({Widget page, UserModel user}) {
+    return Provider<UserModel>.value(
+      value: user,
+      child: MaterialApp(
+        home: page,
+      ),
+    );
   }
 
   testWidgets('test if create trip page is loaded', (WidgetTester tester) async {
     final testKey = Key('create_trip');
-
-    MockAuth mockAuth = MockAuth();
     Widget page = CreateTrip();
 
-    await tester.pumpWidget(makeTestableWidget(child: page, auth: mockAuth));
+    MockUser mockUser = MockUser();
+    when(mockUser.uid).thenReturn('uid');
+    expect(mockUser.uid, 'uid');
+
+    await tester.pumpWidget(makeTestableWidget(page: page, user: mockUser));
 
     expect(find.byKey(testKey), findsOneWidget);
   });
 
   testWidgets('test if form instance is found', (WidgetTester tester) async {
     Widget page = CreateTrip();
-    MockAuth mockAuth = MockAuth();
 
-    await tester.pumpWidget(makeTestableWidget(child: page, auth: mockAuth));
+    MockUser mockUser = MockUser();
+    when(mockUser.uid).thenReturn('uid');
+    expect(mockUser.uid, 'uid');
+
+    await tester.pumpWidget(makeTestableWidget(page: page, user: mockUser));
 
     // verify that form is present
     expect(find.byType(Form), findsOneWidget);
@@ -39,9 +45,12 @@ void main() {
 
   testWidgets('test if all form fields are present', (WidgetTester tester) async {
     Widget page = CreateTrip();
-    MockAuth mockAuth = MockAuth();
 
-    await tester.pumpWidget(makeTestableWidget(child: page, auth: mockAuth));
+    MockUser mockUser = MockUser();
+    when(mockUser.uid).thenReturn('uid');
+    expect(mockUser.uid, 'uid');
+
+    await tester.pumpWidget(makeTestableWidget(page: page, user: mockUser));
 
     expect(find.text('Trip Details'), findsOneWidget);
     expect(find.text('General Information'), findsOneWidget);
@@ -53,9 +62,12 @@ void main() {
 
   testWidgets('test if all form fields are present', (WidgetTester tester) async {
     Widget page = CreateTrip();
-    MockAuth mockAuth = MockAuth();
 
-    await tester.pumpWidget(makeTestableWidget(child: page, auth: mockAuth));
+    MockUser mockUser = MockUser();
+    when(mockUser.uid).thenReturn('uid');
+    expect(mockUser.uid, 'uid');
+
+    await tester.pumpWidget(makeTestableWidget(page: page, user: mockUser));
 
     expect(find.byKey(Key('image_icon')), findsNWidgets(10));
   });
