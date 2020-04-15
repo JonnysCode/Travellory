776689656bb05687cf3664e4f2b5d7367de50c33
antilyas <antilyas@students.zhaw.ch>
2020-04-15 23:26:24 +0200

    + added service locator

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:logger/logger.dart';
+import 'package:travellory/service_locator.dart';
 import 'myapp.dart';
 
 void main() {
   Logger.level = Level.info;
+  setupLocator();
   runApp(MyApp());
 }
