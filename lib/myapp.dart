071148a314bc12fa79ad166448aed999468a3c53
schinsev <schinsev@students.zhaw.ch>
2020-04-16 13:45:18 +0200

    add a search bar

--- lib/myapp.dart
+++ lib/myapp.dart
@@ -6,6 +6,7 @@ import 'package:travellory/screens/authenticate/authenticate.dart';
 import 'package:travellory/screens/authenticate/password.dart';
 import 'package:travellory/screens/authenticate/register.dart';
 import 'package:travellory/screens/authenticate/sign_in.dart';
+import 'package:travellory/screens/friends/create_search_friend_screen.dart';
 import 'package:travellory/screens/home/home.dart';
 import 'package:travellory/screens/home/pages/friends_page.dart';
 import 'package:travellory/screens/trip/bookings/accommodation.dart';
@@ -53,6 +54,7 @@ class MyApp extends StatelessWidget {
             '/booking/publicTransport': (context) => PublicTransport(),
             '/booking/activity': (context) => Activity(),
             '/friends/friends_page': (context) => FriendsPage(),
+            '/search_friends': (context) => SearchFriendsPage(),
           },
         ),
       ),
