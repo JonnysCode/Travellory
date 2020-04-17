494f1de4fcb56a50021191e492a2f518c9035fa9
antilyas <yasmine.antille@gmail.com>
2020-04-17 10:10:25 +0200

    + added error message to on submit trip method

--- lib/services/database/submit.dart
+++ lib/services/database/submit.dart
@@ -4,31 +4,32 @@ import 'package:travellory/models/trip_model.dart';
 import 'package:travellory/services/database/add_database.dart';
 import 'package:travellory/widgets/forms/show_dialog.dart';
 
+final String errorMessage = "Seems like there's a connection problem. "
+    "Please check your internet connection and try submitting again.";
+
 Function() onSubmitBooking(Model model, String functionName, BuildContext context, alertText) {
-  DatabaseAdder databaseAdder = DatabaseAdder();
+  final DatabaseAdder databaseAdder = DatabaseAdder();
 
   return () async {
     final bool added = await databaseAdder.addModel(model, functionName);
     if (added) {
       showSubmittedBookingDialog(context, alertText);
     } else {
-      addToDataBaseFailedDialog(
-          context,
-          "Seems like there's a connection problem. "
-          "Please check your internet connection and try submitting again.");
+      addToDataBaseFailedDialog(context, errorMessage);
     }
   };
 }
 
-void Function() onSubmitTrip(TripModel tripModel, String functionName, BuildContext context, alertText) {
-  DatabaseAdder databaseAdder = DatabaseAdder();
+void Function() onSubmitTrip(
+    TripModel tripModel, String functionName, BuildContext context, alertText) {
+  final DatabaseAdder databaseAdder = DatabaseAdder();
 
   return () async {
     final bool added = await databaseAdder.addModel(tripModel, functionName);
     if (added) {
       showSubmittedTripDialog(context, alertText);
     } else {
-      // TODO(antilyas): add error message to user
+      addToDataBaseFailedDialog(context, errorMessage);
     }
   };
 }
