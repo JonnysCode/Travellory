32aee997d1445b800ccfa4448906b583c9d7c57a
grussjon <jonathangruss@hotmail.com>
2020-04-18 15:50:44 +0200

    * Fix tests

--- test/widget_test/yearly_calendar_test.dart
+++ test/widget_test/yearly_calendar_test.dart
@@ -7,8 +7,6 @@ import 'package:travellory/models/user_model.dart';
 import 'package:travellory/providers/trips_provider.dart';
 import 'package:travellory/screens/home/pages/calendar_page.dart';
 
-class MockTripsProvider extends Mock implements TripsProvider{}
-
 void main() {
   Widget makeTestableWidget({Widget body, TripsProvider tripsProvider}) {
     return ChangeNotifierProvider<TripsProvider>.value(
@@ -23,11 +21,10 @@ void main() {
 
   testWidgets('test if CalendarPage is loaded', (WidgetTester tester) async {
     Widget page = CalendarPage();
-    // Mock the trips provider to return an empty TripModel list
-    MockTripsProvider mockTripsProvider = MockTripsProvider();
-    when(mockTripsProvider.trips).thenReturn(<TripModel>[]);
+
+    TripsProvider tripsProvider = TripsProvider();
     // Build our app and trigger a frame.
-    await tester.pumpWidget(makeTestableWidget(body: page, tripsProvider: mockTripsProvider));
+    await tester.pumpWidget(makeTestableWidget(body: page, tripsProvider: tripsProvider));
 
     // Verify that the CalendarPage is present.
     expect(find.byKey(Key('calendar_page')), findsOneWidget);
@@ -36,11 +33,10 @@ void main() {
 
   testWidgets('test if navigation is present', (WidgetTester tester) async {
     Widget page = CalendarPage();
-    // Mock the trips provider to return an empty TripModel list
-    MockTripsProvider mockTripsProvider = MockTripsProvider();
-    when(mockTripsProvider.trips).thenReturn(<TripModel>[]);
+    
+    TripsProvider tripsProvider = TripsProvider();
     // Build our app and trigger a frame.
-    await tester.pumpWidget(makeTestableWidget(body: page, tripsProvider: mockTripsProvider));
+    await tester.pumpWidget(makeTestableWidget(body: page, tripsProvider: tripsProvider));
 
     // Verify that yearly calendar is present.
     expect(find.byKey(Key('yearly_calendar')), findsOneWidget);
