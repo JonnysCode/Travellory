d695b2a2f3f8112af377842c30bade96308db6c7
grussjon <grussjon@students.zhaw.ch>
2020-04-18 09:31:10 +0200

    * Fix the yearly calendar test

--- test/widget_test/yearly_calendar_test.dart
+++ test/widget_test/yearly_calendar_test.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
 import 'package:provider/provider.dart';
+import 'package:travellory/models/trip_model.dart';
 import 'package:travellory/models/user_model.dart';
+import 'package:travellory/providers/trips_provider.dart';
 import 'package:travellory/screens/home/pages/calendar_page.dart';
 
+class MockTripsProvider extends Mock implements TripsProvider{}
 
 void main() {
-  Widget makeTestableWidget({Widget body}) {
-    UserModel user = UserModel(uid: 'uid', displayName: 'name');
-    return Provider<UserModel>.value(
-      value: user,
+  Widget makeTestableWidget({Widget body, TripsProvider tripsProvider}) {
+    return ChangeNotifierProvider<TripsProvider>.value(
+      value: tripsProvider,
       child: MaterialApp(
         home: Scaffold(
           body: body,
@@ -20,9 +23,11 @@ void main() {
 
   testWidgets('test if CalendarPage is loaded', (WidgetTester tester) async {
     Widget page = CalendarPage();
-
+    // Mock the trips provider to return an empty TripModel list
+    MockTripsProvider mockTripsProvider = MockTripsProvider();
+    when(mockTripsProvider.trips).thenReturn(<TripModel>[]);
     // Build our app and trigger a frame.
-    await tester.pumpWidget(makeTestableWidget(body: page));
+    await tester.pumpWidget(makeTestableWidget(body: page, tripsProvider: mockTripsProvider));
 
     // Verify that the CalendarPage is present.
     expect(find.byKey(Key('calendar_page')), findsOneWidget);
@@ -31,9 +36,11 @@ void main() {
 
   testWidgets('test if navigation is present', (WidgetTester tester) async {
     Widget page = CalendarPage();
-
+    // Mock the trips provider to return an empty TripModel list
+    MockTripsProvider mockTripsProvider = MockTripsProvider();
+    when(mockTripsProvider.trips).thenReturn(<TripModel>[]);
     // Build our app and trigger a frame.
-    await tester.pumpWidget(makeTestableWidget(body: page));
+    await tester.pumpWidget(makeTestableWidget(body: page, tripsProvider: mockTripsProvider));
 
     // Verify that yearly calendar is present.
     expect(find.byKey(Key('yearly_calendar')), findsOneWidget);
