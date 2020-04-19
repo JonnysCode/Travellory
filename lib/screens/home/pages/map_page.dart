62d156c2c7dcdb115312f5bf89b602c6eb430ea6
Dávid Isztl <isztl.david@gmail.com>
2020-04-19 11:37:25 +0200

    bugfixes and new borders

--- lib/screens/home/pages/map_page.dart
+++ lib/screens/home/pages/map_page.dart
@@ -8,7 +8,7 @@ import 'package:travellory/utils/g_map/g_map_border_loader.dart';
 import 'package:travellory/services/api/map/locations.dart' as locations;
 
 String _mapStyle;
-final List<String> _userCities = ["switzerland"];
+final List<String> _userCities = ["switzerland","germany","belgium"];
 
 class MapPage extends StatefulWidget {
   @override
