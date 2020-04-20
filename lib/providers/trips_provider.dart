2530dea090a55a8d2069ad3c0a1033f1c6e18ec1
Nadine Stadelmann <stadena1@students.zhaw.ch>
2020-04-20 14:24:17 +0200

    * modified functions for booking (get/add)

--- lib/providers/trips_provider.dart
+++ lib/providers/trips_provider.dart
@@ -1,39 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:travellory/models/accommodation_model.dart';
+import 'package:travellory/models/flight_model.dart';
+import 'package:travellory/models/public_transport_model.dart';
+import 'package:travellory/models/rental_car_model.dart';
 import 'package:travellory/models/trip_model.dart';
 import 'package:travellory/models/user_model.dart';
 import 'package:travellory/services/database/add_database.dart';
 import 'package:travellory/services/database/get_database.dart';
 import 'package:pedantic/pedantic.dart';
 
-class TripsProvider extends ChangeNotifier{
-  TripsProvider(){
-   _trips = <TripModel>[];
+class TripsProvider extends ChangeNotifier {
+  TripsProvider() {
+    _trips = <TripModel>[];
+    _flights = <FlightModel>[];
+    _accommodations = <AccommodationModel>[];
+    _rentalcars = <RentalCarModel>[];
+    _publictransports = <PublicTransportModel>[];
   }
+
   final DatabaseAdder _databaseAdder = DatabaseAdder();
   final DatabaseGetter _databaseGetter = DatabaseGetter();
 
   bool isFetching = false;
 
   List<TripModel> _trips;
+  List<FlightModel> _flights;
+  List<AccommodationModel> _accommodations;
+  List<RentalCarModel> _rentalcars;
+  List<PublicTransportModel> _publictransports;
   UserModel _user;
+
   // TODO: next upcoming trip
   // TODO: selected trip so we don't have to pass it via Navigator
 
   List<TripModel> get trips => _trips;
 
-  set user(UserModel user){
+  List<FlightModel> get flights => _flights;
+
+  List<AccommodationModel> get accommodations => _accommodations;
+
+  List<RentalCarModel> get rentalcars => _rentalcars;
+
+  List<PublicTransportModel> get publictransports => _publictransports;
+
+  set user(UserModel user) {
     _user = user;
   }
 
-  void init(UserModel user){
+  void init(UserModel user) {
     _user = user;
     unawaited(_fetchTrips());
   }
 
   Future<bool> addTrip(TripModel tripModel) async {
     tripModel.userUID = _user.uid;
-    final bool added = await _databaseAdder.addModel(tripModel, _databaseAdder.addTrip);
-    if(added){
+    final bool added =
+        await _databaseAdder.addModel(tripModel, _databaseAdder.addTrip);
+    if (added) {
       unawaited(_fetchTrips());
     }
     return added;
@@ -41,7 +64,26 @@ class TripsProvider extends ChangeNotifier{
 
   Future<void> _fetchTrips() async {
     isFetching = true;
-    _trips = await _databaseGetter.getTripsFromDatabase(_user.uid);
+    _trips = await _databaseGetter.getEntriesFromDatabase(
+        _user.uid, _databaseGetter.getTrips);
+    isFetching = false;
+    notifyListeners();
+  }
+
+  void initBookings(TripModel trip) {
+    unawaited(_fetchBookings(trip.uid));
+  }
+
+  Future<void> _fetchBookings(String tripUID) async {
+    isFetching = true;
+    _flights = await _databaseGetter.getEntriesFromDatabase(
+        tripUID, _databaseGetter.getFlights);
+    _accommodations = await _databaseGetter.getEntriesFromDatabase(
+        tripUID, _databaseGetter.getAccommodations);
+    _rentalcars = await _databaseGetter.getEntriesFromDatabase(
+        tripUID, _databaseGetter.getRentalCars);
+    _publictransports = await _databaseGetter.getEntriesFromDatabase(
+        tripUID, _databaseGetter.getPublicTransportations);
     isFetching = false;
     notifyListeners();
   }
--- lib/services/database/get_database.dart
+++ lib/services/database/get_database.dart
@@ -1,17 +1,27 @@
 import 'package:cloud_functions/cloud_functions.dart';
 import 'package:travellory/logger.dart';
+import 'package:travellory/models/abstract_model.dart';
+import 'package:travellory/models/accommodation_model.dart';
+import 'package:travellory/models/flight_model.dart';
+import 'package:travellory/models/public_transport_model.dart';
+import 'package:travellory/models/rental_car_model.dart';
 import 'package:travellory/models/trip_model.dart';
 
 class DatabaseGetter{
+  final String getTrips = 'trips-getTrips';
+  final String getFlights = 'booking-getFlights';
+  final String getAccommodations = 'booking-getAccommodations';
+  final String getRentalCars = 'booking-getRentalCars';
+  final String getPublicTransportations = 'booking-getPublicTransportations';
   final log = getLogger('DatabaseGetter');
 
-  Future<List<TripModel>> getTripsFromDatabase(String userUID) async {
+  Future<List<Model>> getEntriesFromDatabase(String uid, String function) async {
     final HttpsCallable callable =
-    CloudFunctions.instance.getHttpsCallable(functionName: 'trips-getTrips');
-    List<dynamic> trips = [];
+    CloudFunctions.instance.getHttpsCallable(functionName: function);
+    List<dynamic> entries = [];
     try {
-      final HttpsCallableResult result = await callable.call(_getUserMap(userUID));
-      trips = result.data;
+      final HttpsCallableResult result = await callable.call(_getMap(uid, function));
+      entries = result.data;
     } on CloudFunctionsException catch (e) {
       log.e('caught firebase functions exception');
       log.e(e.code);
@@ -21,15 +31,42 @@ class DatabaseGetter{
       log.w('caught generic exception');
       log.w(e);
     }
-    return _createTrips(trips);
+    return _createEntries(entries, function);
   }
 
-  Map<String, dynamic> _getUserMap(String userUID) {
-    return {"userUID": userUID};
+  Map<String, dynamic> _getMap(String uid, String function) {
+    switch (function) {
+      case "trips-getTrips":
+        return {"userUID": uid};
+        break;
+    }
+    return {"tripUID": uid};
+  }
+
+  List<Model> _createEntries(dbEntries, function) {
+    List<Model> entries = <Model>[];
+    switch (function) {
+      case "booking-getFlights":
+        entries = _createFlights(dbEntries);
+        break;
+      case "booking-getAccommodations":
+        entries = _createAccommodations(dbEntries);
+        break;
+      case "booking-getRentalCars":
+        entries = _createRentalCars(dbEntries);
+        break;
+      case "booking-getPublicTransportations":
+        entries = _createPublicTransports(dbEntries);
+        break;
+      case "trips-getTrips":
+        entries = _createTrips(dbEntries);
+        break;
+    }
+    return entries;
   }
 
   List<TripModel> _createTrips(dbTrips) {
-    // add to trips from DB to tripModels
+    // add trips from DB to tripModels
     List<TripModel> trips = <TripModel>[];
     for (var dbTrip in dbTrips) {
       TripModel trip = TripModel.fromData(dbTrip);
@@ -37,7 +74,44 @@ class DatabaseGetter{
     }
     return trips;
   }
+
+  List<FlightModel> _createFlights(dbFlights) {
+    // add flights from DB to flightModels
+    List<FlightModel> flights = <FlightModel>[];
+    for (var dbFlight in dbFlights) {
+      FlightModel flight = FlightModel.fromData(dbFlight);
+      flights.add(flight);
+    }
+    return flights;
+  }
+
+  List<AccommodationModel> _createAccommodations(dbAccommodations) {
+    // add accommodations from DB to accommodationModels
+    List<AccommodationModel> accommodations = <AccommodationModel>[];
+    for (var dbAccommodation in dbAccommodations) {
+      AccommodationModel accommodation = AccommodationModel.fromData(dbAccommodation);
+      accommodations.add(accommodation);
+    }
+    return accommodations;
+  }
+
+  List<RentalCarModel> _createRentalCars(dbRentalCars) {
+    // add rentalcars from DB to rentalcarModels
+    List<RentalCarModel> rentalCars = <RentalCarModel>[];
+    for (var dbRentalCar in dbRentalCars) {
+      RentalCarModel rentalCar = RentalCarModel.fromData(dbRentalCar);
+      rentalCars.add(rentalCar);
+    }
+    return rentalCars;
+  }
+
+  List<PublicTransportModel> _createPublicTransports(dbPublicTransports) {
+    // add publictransport from DB to publictransportModels
+    List<PublicTransportModel> publicTransports = <PublicTransportModel>[];
+    for (var dbPublicTransport in dbPublicTransports) {
+      PublicTransportModel publicTransport = PublicTransportModel.fromData(dbPublicTransport);
+      publicTransports.add(publicTransport);
+    }
+    return publicTransports;
+  }
 }
-
-
-
