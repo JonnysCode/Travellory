86fe5d8c205e65be39b244721abc76ab3e441bd0
schinsev <schinsev@students.zhaw.ch>
2020-04-21 08:27:53 +0200

    create search bar

--- lib/screens/friends/create_search_friend_screen.dart
+++ lib/screens/friends/create_search_friend_screen.dart
@@ -1,6 +1,12 @@
 import 'package:flappy_search_bar/flappy_search_bar.dart';
+import 'package:flappy_search_bar/search_bar_style.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class Post {
+  final String title;
+
+  Post(this.title);
+}
 
 class SearchFriendsPage extends StatefulWidget {
   @override
@@ -8,31 +14,28 @@ class SearchFriendsPage extends StatefulWidget {
 }
 
 class _SearchFriendsPageState extends State<SearchFriendsPage> {
+  Future<List<Post>> search(String search) async {
+    await Future.delayed(Duration(seconds: 1));
+    return List.generate(search.length, (int index) {
+      return Post(
+        'Username : $search $index',
+      );
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-          key: Key('search_friends'),
-          child: Stack(children: <Widget>[
-            SizedBox.expand(
-                child: Padding(
-                    padding: const EdgeInsets.fromLTRB(6, 40, 6, 0),
-                    child: Container(
-                        alignment: AlignmentDirectional(-1, -1),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius:
-                              BorderRadius.vertical(top: Radius.circular(40.0)),
-                          boxShadow: [
-                            BoxShadow(
-                                blurRadius: 20,
-                                color: Colors.black.withOpacity(.2),
-                                offset: Offset(0.0, -6.0))
-                          ],
-                        ),
-                        child: Container(
+        body: SafeArea(
+            key: Key('search_friends'),
+            child: Stack(children: <Widget>[
+              SizedBox.expand(
+                  child: Padding(
+                      padding: const EdgeInsets.fromLTRB(6, 40, 6, 0),
+                      child: Container(
+                          alignment: AlignmentDirectional(-1, -1),
                           decoration: BoxDecoration(
-                            color: Theme.of(context).primaryColor,
+                            color: Colors.white,
                             borderRadius: BorderRadius.vertical(
                                 top: Radius.circular(40.0)),
                             boxShadow: [
@@ -42,27 +45,67 @@ class _SearchFriendsPageState extends State<SearchFriendsPage> {
                                   offset: Offset(0.0, -6.0))
                             ],
                           ),
-                          constraints: BoxConstraints(
-                            maxHeight: 90.0,
-                            maxWidth: 365.0,
-                          ),
-                          child: //Row(children: [
-                           /* IconButton(
-                              onPressed: () => Navigator.pop(context),
-                              icon: FaIcon(FontAwesomeIcons.arrowAltCircleLeft),
-                              iconSize: 30,
-                              color: Colors.red,
-                            ),*/
-                            SearchBar(
-                              searchBarPadding:
-                                  EdgeInsets.symmetric(horizontal: 20),
-                              headerPadding:
-                                  EdgeInsets.symmetric(horizontal: 10),
-                              listPadding: EdgeInsets.symmetric(horizontal: 10),
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Theme.of(context).primaryColor,
+                              borderRadius: BorderRadius.vertical(
+                                  top: Radius.circular(40.0)),
+                              boxShadow: [
+                                BoxShadow(
+                                    blurRadius: 20,
+                                    color: Colors.black.withOpacity(.2),
+                                    offset: Offset(0.0, -6.0))
+                              ],
                             ),
-
-                        ))))
-          ])),
-    );
+                            constraints: BoxConstraints(
+                              maxHeight: 80.0,
+                              maxWidth: 365.0,
+                            ),
+                            child: Row(children: [
+                              Container(
+                                child: IconButton(
+                                  onPressed: () => Navigator.pop(context),
+                                  icon: Icon(Icons.arrow_back),
+                                  iconSize: 30,
+                                  color: Colors.black,
+                                ),
+                              ),
+                              Expanded(
+                                child: SearchBar(
+                                    key: Key('search_bar'),
+                                    onSearch: search,
+                                    onItemFound: (Post post, int index) {
+                                      return ListTile(
+                                        title: Text(post.title),
+                                      );
+                                    },
+                                    searchBarPadding:
+                                        EdgeInsets.fromLTRB(0, 0, 10, 0),
+                                    headerPadding:
+                                        EdgeInsets.symmetric(horizontal: 50),
+                                    listPadding:
+                                        EdgeInsets.symmetric(horizontal: 30),
+                                    hintText: 'Add friends',
+                                    hintStyle: TextStyle(
+                                      color: Colors.grey[800],
+                                    ),
+                                    icon: Icon(
+                                      Icons.search,
+                                      size: 30.0,
+                                    ),
+                                    searchBarStyle: SearchBarStyle(
+                                      backgroundColor:
+                                          Theme.of(context).primaryColor,
+                                      padding: EdgeInsets.all(5),
+                                    ),
+                                    cancellationWidget: Text(
+                                      'Cancel',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    )),
+                              ),
+                            ]),
+                          ))))
+            ])));
   }
 }
--- lib/screens/home/pages/friends_page.dart
+++ lib/screens/home/pages/friends_page.dart
@@ -11,68 +11,68 @@ class _FriendsPageState extends State<FriendsPage> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      key: Key('friends_page'),
+        key: Key('friends_page'),
         child: Stack(children: <Widget>[
-      SizedBox.expand(
-          child: Padding(
-              padding: const EdgeInsets.fromLTRB(6, 40, 6, 0),
-              child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius:
-                        BorderRadius.vertical(top: Radius.circular(40.0)),
-                    boxShadow: [
-                      BoxShadow(
-                          blurRadius: 20,
-                          color: Colors.black.withOpacity(.2),
-                          offset: Offset(0.0, -6.0))
-                    ],
-                  ),
-                  child: Column(children: <Widget>[
-                    Padding(
-                        padding:
-                            const EdgeInsets.only(top: 15, left: 40, right: 25),
-                        child: Container(
-                          height: 56,
-                          width: MediaQuery.of(context).size.width,
-                          child: Stack(
-                            children: <Widget>[
-                              Container(
-                                width: 160,
-                                child: FashionFetishText(
-                                  text: 'Friends',
-                                  size: 22,
-                                  height: 1.15,
-                                  fontWeight: FashionFontWeight.heavy,
-                                ),
-                              ),
-                              Positioned(
-                                top: 24,
-                                right: 36,
-                                child: FashionFetishText(
-                                  text: 'Search',
-                                  size: 16,
-                                  color: Colors.black54,
-                                  fontWeight: FashionFontWeight.bold,
-                                ),
-                              ),
-                              Positioned(
-                                top: 11,
-                                right: 0,
-                                child: GestureDetector(
-                                  onTap: () => Navigator.pushNamed(
-                                      context, '/search_friends'),
-                                  child: Icon(
-                                    FontAwesomeIcons.search,
-                                    color: Theme.of(context).primaryColor,
-                                    size: 32,
+          SizedBox.expand(
+              child: Padding(
+                  padding: const EdgeInsets.fromLTRB(6, 40, 6, 0),
+                  child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius:
+                            BorderRadius.vertical(top: Radius.circular(40.0)),
+                        boxShadow: [
+                          BoxShadow(
+                              blurRadius: 20,
+                              color: Colors.black.withOpacity(.2),
+                              offset: Offset(0.0, -6.0))
+                        ],
+                      ),
+                      child: Column(children: <Widget>[
+                        Padding(
+                            padding: const EdgeInsets.only(
+                                top: 15, left: 40, right: 25),
+                            child: Container(
+                              height: 56,
+                              width: MediaQuery.of(context).size.width,
+                              child: Stack(
+                                children: <Widget>[
+                                  Container(
+                                    width: 160,
+                                    child: FashionFetishText(
+                                      text: 'Friends',
+                                      size: 22,
+                                      height: 1.15,
+                                      fontWeight: FashionFontWeight.heavy,
+                                    ),
                                   ),
-                                ),
+                                  Positioned(
+                                    top: 24,
+                                    right: 36,
+                                    child: FashionFetishText(
+                                      text: 'Search',
+                                      size: 16,
+                                      color: Colors.black54,
+                                      fontWeight: FashionFontWeight.bold,
+                                    ),
+                                  ),
+                                  Positioned(
+                                    top: 11,
+                                    right: 0,
+                                    child: GestureDetector(
+                                      onTap: () => Navigator.pushNamed(
+                                          context, '/search_friends'),
+                                      child: Icon(
+                                        FontAwesomeIcons.search,
+                                        color: Theme.of(context).primaryColor,
+                                        size: 32,
+                                      ),
+                                    ),
+                                  ),
+                                ],
                               ),
-                            ],
-                          ),
-                        ))
-                  ]))))
-    ]));
+                            ))
+                      ]))))
+        ]));
   }
 }
