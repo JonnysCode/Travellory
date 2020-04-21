0266a8e9da9d35d9d4031a3eb5afc6f60a50eb3f
Nadine Stadelmann <stadena1@students.zhaw.ch>
2020-04-21 15:52:30 +0200

    * added activitiy connection to backend

--- lib/services/database/get_database.dart
+++ lib/services/database/get_database.dart
@@ -2,6 +2,7 @@ import 'package:cloud_functions/cloud_functions.dart';
 import 'package:travellory/logger.dart';
 import 'package:travellory/models/abstract_model.dart';
 import 'package:travellory/models/accommodation_model.dart';
+import 'package:travellory/models/activity_model.dart';
 import 'package:travellory/models/flight_model.dart';
 import 'package:travellory/models/public_transport_model.dart';
 import 'package:travellory/models/rental_car_model.dart';
@@ -11,6 +12,7 @@ class DatabaseGetter {
   final String getTrips = 'trips-getTrips';
   final String getFlights = 'booking-getFlights';
   final String getAccommodations = 'booking-getAccommodations';
+  final String getActivities = 'activity-getActivities';
   final String getRentalCars = 'booking-getRentalCars';
   final String getPublicTransportations = 'booking-getPublicTransportations';
   final log = getLogger('DatabaseGetter');
@@ -24,7 +26,6 @@ class DatabaseGetter {
       final HttpsCallableResult result =
           await callable.call(_getMap(uid, function));
       if (result.data.contains('no-data')) {
-        print("good");
         return _getEmptyEntries(function);
       }
       entries = result.data;
@@ -64,6 +65,9 @@ class DatabaseGetter {
       case "booking-getPublicTransportations":
         entries = <PublicTransportModel>[];
         break;
+      case "activity-getActivities":
+        entries = <ActivityModel>[];
+        break;
       case "trips-getTrips":
         entries = <TripModel>[];
         break;
@@ -85,6 +89,9 @@ class DatabaseGetter {
         break;
       case "booking-getPublicTransportations":
         entries = _createPublicTransports(dbEntries);
+        break;
+      case "activity-getActivities":
+        entries = _createActivities(dbEntries);
         break;
       case "trips-getTrips":
         entries = _createTrips(dbEntries);
@@ -143,5 +150,16 @@ class DatabaseGetter {
       publicTransports.add(publicTransport);
     }
     return publicTransports;
+  }
+
+  List<ActivityModel> _createActivities(dbActivities) {
+    // add activity from DB to activityModels
+    List<ActivityModel> activities = <ActivityModel>[];
+    for (var dbActivity in dbActivities) {
+      ActivityModel activity =
+      ActivityModel.fromData(dbActivity);
+      activities.add(activity);
+    }
+    return activities;
   }
 }
