6244324519d016f40adc84ed174b27828fc3ab33
bertaben <bertaben@students.zhaw.ch>
2020-04-22 09:20:52 +0200

    new methods & comments implemented updatePhotoUrl in AuthService added comments to storage class

--- lib/services/storage.dart
+++ lib/services/storage.dart
@@ -3,27 +3,33 @@ import 'package:path/path.dart' as path;
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:travellory/logger.dart';
 
+
 class Storage{
-
-
 
   static final String USER_PROFILE_PICTURES = 'profile-pictures/';
 
+  /// this method can be called to upload files to the firebase storage.
+  /// it takes 2 parameter (optional 3rd)
+  /// file: the file to upload
+  /// directory: the directory on the firebase storage
+  /// (optional) filename: the filename with which it should be uploaded
   static Future<String> uploadFile(File file, String directory, {String filename}) async {
     final log = getLogger('Storage');
 
     if(filename == null){
+      log.d('no filename provided, will use original file name');
       filename = path.basename(file.path);
     }
-    log.d("File: "+file.path);
+    log.d("Will upload file: "+filename);
     StorageReference storageReference = FirebaseStorage.instance
         .ref().child(directory+'/'+filename);
 
     final StorageUploadTask uploadTask = storageReference.putFile(file);
     await uploadTask.onComplete;
-    String fileURL = await storageReference.getDownloadURL();
 
-    log.d("File: "+file.path);
+    String fileURL = await storageReference.getDownloadURL();
+    log.d("FileURL of uploaded file: "+fileURL);
+
     return fileURL;
   }
 }
