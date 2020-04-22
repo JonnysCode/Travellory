0b89420cded99018f7551ea2e186ccb59a3311c3
hessgia1 <hessgia1@students.zhaw.ch>
2020-04-22 21:58:19 +0200

    Friendspage backend

---
+++ lib/providers/friends_provider.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:travellory/models/friends_model.dart';
+import 'package:travellory/models/user_model.dart';
+import 'package:pedantic/pedantic.dart';
+import 'package:travellory/services/friend_management.dart';
+
+class FriendsProvider extends ChangeNotifier{
+  FriendsProvider(){
+    _friends = <FriendsModel>[];
+    _friendRequests = <FriendsModel>[];
+  }
+
+  bool isFetching = true;
+
+  List<FriendsModel> _friends;
+  List<FriendsModel> _friendRequests;
+  UserModel _user;
+
+  List<FriendsModel> get friends => _friends;
+  List<FriendsModel> get friendRequests => _friendRequests;
+
+  set user(UserModel user){
+    _user = user;
+  }
+
+  void init(UserModel user){
+    _user = user;
+    unawaited(_fetchFriends());
+  }
+
+  Future<void> _fetchFriends() async {
+    isFetching = true;
+    _friends = await FriendManagement.getFriends(_user.uid);
+    _friendRequests = await FriendManagement.getFriendRequests(_user.uid);
+    isFetching = false;
+    notifyListeners();
+  }
+}
