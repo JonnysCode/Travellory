07687e9bd827ab0e35c6a0f4e50aa1073981f962
bertaben <bertaben@students.zhaw.ch>
2020-04-22 17:23:56 +0200

    optimized UserModel + code formatting

--- lib/services/storage.dart
+++ lib/services/storage.dart
@@ -4,25 +4,26 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:travellory/logger.dart';
 
 final String USER_PROFILE_PICTURES = 'profile-pictures/';
-final String DEFAULT_USER_PROFILE_PICTURES = 'https://firebasestorage.googleapis.com/v0/b/travellory-9b789.appspot.com/o/profile-pictures%2Fphoto_camera.png?alt=media&token=5e138c64-709e-4172-9b89-664cdc28a347';
+final String DEFAULT_USER_PROFILE_PICTURES =
+    'https://firebasestorage.googleapis.com/v0/b/travellory-9b789.appspot.com/o/profile-pictures%2Fphoto_camera.png?alt=media&token=5e138c64-709e-4172-9b89-664cdc28a347';
 
-class Storage{
-
+class Storage {
   /// this method can be called to upload files to the firebase storage.
   /// it takes 2 parameter (optional 3rd)
   /// file: the file to upload
   /// directory: the directory on the firebase storage
   /// (optional) filename: the filename with which it should be uploaded
-  Future<String> uploadFile(File file, String directory, {String filename}) async {
+  Future<String> uploadFile(File file, String directory,
+      {String filename}) async {
     final log = getLogger('Storage');
 
-    if(filename == null){
+    if (filename == null) {
       log.d('no filename provided, will use original file name');
       filename = path.basename(file.path);
     }
     log.d('Will upload file: $filename');
-    StorageReference storageReference = FirebaseStorage.instance
-        .ref().child(directory+'/'+filename);
+    StorageReference storageReference =
+        FirebaseStorage.instance.ref().child(directory + '/' + filename);
 
     final StorageUploadTask uploadTask = storageReference.putFile(file);
     await uploadTask.onComplete;
@@ -33,6 +34,3 @@ class Storage{
     return fileURL;
   }
 }
-
-
-
