fbc01f5e8ebfda883c68d84db139798a8f3cf5e8
grussjon <jonathangruss@hotmail.com>
2020-04-26 20:35:30 +0200

    * isSearching setup

--- lib/screens/home/pages/friends_page.dart
+++ lib/screens/home/pages/friends_page.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:travellory/providers/screens/friends_page_provider.dart';
 import 'package:travellory/screens/friends/friends_list_page_dev.dart';
 import 'package:travellory/screens/friends/search_friend_page.dart';
 import 'package:travellory/screens/friends/friends_list_page.dart';
-
-bool isSearch;
 
 class FriendsPage extends StatefulWidget {
   @override
@@ -21,34 +21,38 @@ class _FriendsPageState extends State<FriendsPage> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-        child: Container(
-            key: Key('friends_page'),
-            child: Stack(
-                children: <Widget>[
-                  SizedBox.expand(
-                    child: Padding(
-                      padding: const EdgeInsets.fromLTRB(6, 40, 6, 0),
-                      child: Container(
-                          child: isSearch
-                              ? Container(child: SearchFriendsPage()) // TODO Fix that both pages on the friendspage
-                              : Container(child: _dev
-                                ? Container(child: FriendListPageDev())
-                                : Container(child: FriendListPage()))
+    return ChangeNotifierProvider<FriendsPageProvider>(
+      create: (_) => FriendsPageProvider(),
+      child: SafeArea(
+          child: Container(
+              key: Key('friends_page'),
+              child: SizedBox.expand(
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(6, 40, 6, 0),
+                  child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.vertical(top: Radius.circular(40.0)),
+                        boxShadow: [
+                          BoxShadow(
+                              blurRadius: 20,
+                              color: Colors.black.withOpacity(.2),
+                              offset: Offset(0.0, -6.0))
+                        ],
                       ),
-                    ),
+                    child: Selector<FriendsPageProvider, bool>(
+                      selector: (_, friendsPageProvider) => friendsPageProvider.isSearching,
+                      builder: (_, isSearching, __) => isSearching
+                          ? Container(child: SearchFriendsPage()) // TODO Fix that both pages on the friendspage
+                          : Container(child: _dev
+                            ? Container(child: FriendListPageDev())
+                            : Container(child: FriendListPage())),
+                    )
                   ),
-                  Positioned(
-                    top: 15,
-                    left: 25,
-                    child: Image(
-                      image: AssetImage('assets/images/logo/travellory_icon.png'),
-                      height: 80,
-                    ),
-                  )
-                ]
-            )
-        )
+                ),
+              )
+          )
+      ),
     );
   }
 }
