4e0240e35af826c325c163216866084e4ea36bf4
antilyas <antilyas@students.zhaw.ch>
2020-04-28 10:53:56 +0200

    * edited delete bookings dialog popup

--- lib/services/database/delete_database.dart
+++ lib/services/database/delete_database.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_functions/cloud_functions.dart';
+import 'package:flutter/material.dart';
 import 'package:travellory/logger.dart';
 import 'package:travellory/models/abstract_model.dart';
 import 'package:travellory/models/accommodation_model.dart';
@@ -6,10 +7,14 @@ import 'package:travellory/models/activity_model.dart';
 import 'package:travellory/models/flight_model.dart';
 import 'package:travellory/models/public_transport_model.dart';
 import 'package:travellory/models/rental_car_model.dart';
+import 'package:travellory/widgets/bookings/edit_delete_dialogs.dart';
+import 'package:travellory/widgets/forms/show_dialog.dart';
+
+final log = getLogger('DatabaseDeleter');
 
 class DatabaseDeleter {
   Future<bool> deleteModel(Model model, String correspondingFunctionName) async {
-
+    // TODO(nadine): implement database function
   }
 }
 
@@ -32,6 +37,23 @@ String getDeleteFunctionNameBasedOn(Model model) {
   return functionName;
 }
 
-void Function() onDeleteBooking() {
- // TODO (nadine): implement delete booking function maybe similar to onSubmitTrip ?
+void Function() onDeleteBooking(BuildContext context, String errorMessage) {
+  const String alertText =
+      "You've just deleted this entry. Your booking overview has been updated. ";
+
+  const String errorMessage = "Seems like there's a connection problem. "
+      "Please check your internet connection and try submitting again.";
+
+  // TODO (nadine): implement delete booking function maybe similar to onSubmitTrip ?
+  return () async {
+    // TODO (nadine): implement correct bool
+    final bool added = false;
+    if (added) {
+      showDeletedBookingDialog(context, alertText);
+      log.i('onDeleteBooking was performed');
+    } else {
+      addToDataBaseFailedDialog(context, errorMessage);
+      log.i('onDeleteBooking did not work');
+    }
+  };
 }
