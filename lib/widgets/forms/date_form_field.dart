133c89c68c638f301d021a5c234ac82ffeab480a
antilyas <yasmine.antille@gmail.com>
2020-04-28 23:11:57 +0200

    * updated date form field

--- lib/widgets/forms/date_form_field.dart
+++ lib/widgets/forms/date_form_field.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_rounded_date_picker/rounded_picker.dart';
+import 'package:intl/intl.dart';
 
 class DateFormField extends StatefulWidget {
   const DateFormField(
       {Key key,
       this.icon,
+      this.initialValue,
       this.labelText,
       this.optional = false,
       this.controller,
@@ -15,6 +17,7 @@ class DateFormField extends StatefulWidget {
       : super(key: key);
 
   final Icon icon;
+  final String initialValue;
   final String labelText;
   final bool optional;
   final TextEditingController controller;
@@ -36,12 +39,24 @@ class DateFormFieldState extends State<DateFormField> with AutomaticKeepAliveCli
   TextEditingController controller;
   TextEditingController displayController;
   DateTime selectedDate;
+  DateTime initialDate;
 
   @override
   void initState() {
     super.initState();
     controller = widget.controller != null ? widget.controller : TextEditingController();
     displayController = TextEditingController();
+    // TODO(antilyas): check if dateformating works
+    getInitialDate();
+  }
+
+  DateTime getInitialDate() {
+    if (widget.initialValue != null) {
+      displayController..text = (widget.initialValue);;
+      return initialDate = DateFormat("dd-MM-yyyy", "en_US").parse(widget.initialValue);
+    } else {
+      return DateTime.now();
+    }
   }
 
   @override
@@ -56,16 +71,19 @@ class DateFormFieldState extends State<DateFormField> with AutomaticKeepAliveCli
     final DateTime pickedDate = await showRoundedDatePicker(
       context: context,
       theme: ThemeData.dark(),
-      initialDate: DateTime.now(),
+      initialDate: initialDate,
       firstDate: DateTime(DateTime.now().year - 1),
       lastDate: DateTime(DateTime.now().year + 1),
       borderRadius: 16,
     );
     if (pickedDate != null) {
       selectedDate = pickedDate;
-      final String pickedDateString = pickedDate.toString();
-      displayController.text = "$pickedDateString".split(' ')[0];
-      controller.text = "$pickedDateString".split(' ')[0];
+      // TODO(antilyas): one controller is enough now that dates are saved as strings to database
+//      final String pickedDateString = pickedDate.toString();
+      displayController.text = DateFormat("dd-MM-yyyy").format(pickedDate);
+      controller.text = DateFormat("dd-MM-yyyy").format(pickedDate);
+//      displayController.text = "$pickedDateString".split(' ')[0];
+//      controller.text = "$pickedDateString".split(' ')[0];
       if (widget.chosenDate != null) widget.chosenDate(selectedDate);
       if (widget.chosenDateString != null) widget.chosenDateString(controller.text);
     }
@@ -86,7 +104,8 @@ class DateFormFieldState extends State<DateFormField> with AutomaticKeepAliveCli
                 return widget.validatorText;
               } else if (widget.beforeDateKey != null &&
                   selectedDate.isBefore(widget.beforeDateKey.currentState.selectedDate) &&
-                      (displayController.text != widget.beforeDateKey.currentState.displayController.text)) {
+                  (displayController.text !=
+                      widget.beforeDateKey.currentState.displayController.text)) {
                 return widget.dateValidationMessage;
               }
               return null;
