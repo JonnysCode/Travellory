01d2d1784b009b289f4784be11cab83ae70ee896
Dávid Isztl <isztl.david@gmail.com>
2020-05-01 18:14:06 +0200

    initial buggy real markers

--- lib/screens/home/pages/map_page.dart
+++ lib/screens/home/pages/map_page.dart
@@ -5,8 +5,13 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:provider/provider.dart';
+import 'package:travellory/models/accommodation_model.dart';
+import 'package:travellory/providers/single_trip_provider.dart';
+import 'package:travellory/providers/trips_provider.dart';
+import 'package:travellory/screens/bookings/view_accommodation.dart';
 import 'package:travellory/utils/g_map/g_map_border_loader.dart';
-import 'package:travellory/services/api/map/locations.dart' as locations;
+import 'package:travellory/widgets/trip/trip_card.dart';
 
 String _mapStyle;
 final List<String> _userStates = ["switzerland","austria","belgium"];
@@ -50,11 +55,38 @@ class MapSampleState extends State<MapSample> {
   final Map<String, Marker> _markers = {};
   final List<Polygon> _boundaries = [];
 
+  Future<void> test() async {
+    final List<SingleTripProvider> trips = await Provider.of<TripsProvider>(context, listen: false).trips;
+
+    _markers.clear();
+
+    for (final SingleTripProvider trip in trips) {
+      await trip.tripModel.init();
+      final List<AccommodationModel> accommodations = await trip.accommodations;
+
+      for (final AccommodationModel accommodation in accommodations) {
+        final marker = Marker(
+          markerId: MarkerId(accommodation.name),
+          position: LatLng(accommodation.latitude, accommodation.longitude),
+          infoWindow: InfoWindow(
+            title: accommodation.name,
+            snippet: accommodation.address,
+            onTap: () {
+              AccommodationView();
+            }
+          ),
+        );
+        _markers[accommodation.tripUID] = marker;
+      }
+    }
+  }
+
   Future<void> onMapCreated() async {
-    final googleOffices = await locations.getGoogleOffices();
+    //final googleOffices = await locations.getGoogleOffices();
     final _boundariesTemp = await GMapBorderLoader.generateBorders(_userStates);
 
     setState(() {
+      /*
       _markers.clear();
       for (final office in googleOffices.offices) {
         final marker = Marker(
@@ -72,6 +104,9 @@ class MapSampleState extends State<MapSample> {
         );
         _markers[office.name] = marker;
       }
+       */
+
+      test();
 
       if(_boundariesTemp.isNotEmpty){
         _boundaries.clear();
