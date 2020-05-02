d05ac8aedb2416355fe9c842d2627b2ee1816836
antilyas <yasmine.antille@gmail.com>
2020-05-02 20:33:44 +0200

    * refactored edit and add public transport

--- lib/screens/bookings/add_public_transport.dart
+++ lib/screens/bookings/add_public_transport.dart
@@ -5,6 +5,8 @@ import 'package:travellory/models/public_transport_model.dart';
 import 'package:travellory/models/trip_model.dart';
 import 'package:travellory/providers/single_trip_provider.dart';
 import 'package:travellory/providers/trips_provider.dart';
+import 'package:travellory/services/database/edit.dart';
+import 'package:travellory/services/database/edit_database.dart';
 import 'package:travellory/shared/lists_of_types.dart';
 import 'package:travellory/utils/list_models.dart';
 import 'package:travellory/services/database/submit.dart';
@@ -28,17 +30,11 @@ class PublicTransport extends StatefulWidget {
 class PublicTransportState<T extends PublicTransport> extends State<T> {
   ListModel<Widget> publicTransportList;
   final GlobalKey<FormState> publicTransportFormKey = GlobalKey<FormState>();
-  final PublicTransportModel publicTransportModel = PublicTransportModel();
+
+//  final PublicTransportModel publicTransportModel = PublicTransportModel();
 
   final GlobalKey<AnimatedListState> _listKey = GlobalKey<AnimatedListState>();
   final GlobalKey<DateFormFieldState> _depDateFormFieldKey = GlobalKey<DateFormFieldState>();
-  TravelloryDropdownField transportTypeDropdown;
-  CheckboxFormField bookingMadeCheckbox;
-  CheckboxFormField seatReservedCheckbox;
-
-  Widget typeSpecificationAdditional;
-  Widget bookingMadeAdditional;
-  Widget seatReservationAdditional;
 
   bool validateForm() {
     return publicTransportFormKey.currentState.validate();
@@ -47,144 +43,6 @@ class PublicTransportState<T extends PublicTransport> extends State<T> {
   @override
   void initState() {
     super.initState();
-
-    transportTypeDropdown = TravelloryDropdownField(
-        title: 'Select Transport Type',
-        types: publicTransportTypes,
-        onChanged: (value) {
-          publicTransportModel.transportationType = value.name;
-          showAdditional(publicTransportList, value.name == 'Other', transportTypeDropdown,
-              typeSpecificationAdditional);
-        },
-        validatorText: 'Please enter the required information');
-
-    bookingMadeCheckbox = CheckboxFormField(
-      initialValue: false,
-      label: 'Did you book this public transport?',
-      onChanged: (value) {
-        publicTransportModel.booked = value;
-        showAdditional(publicTransportList, value, bookingMadeCheckbox, bookingMadeAdditional);
-      },
-    );
-
-    seatReservedCheckbox = CheckboxFormField(
-        initialValue: false,
-        label: 'Did you make a seat reservation?',
-        onChanged: (value) {
-          publicTransportModel.seatReserved = value;
-          showAdditional(
-              publicTransportList, value, seatReservedCheckbox, seatReservationAdditional);
-        });
-
-    // don't put in build because it will be recreated on every build
-    // with state changes this is not appreciated
-    final List<Widget> shown = [
-      BookingSiteTitle('Add Public Transport', FontAwesomeIcons.train),
-      SectionTitle('Type of Transportation'),
-      transportTypeDropdown,
-      TravelloryFormField(
-          labelText: 'Company',
-          icon: Icon(FontAwesomeIcons.solidBuilding),
-          optional: true,
-          onChanged: (value) => publicTransportModel.publicTransportCompany = value),
-      SectionTitle('Departure Information'),
-      TravelloryFormField(
-        labelText: 'Departure Location *',
-        icon: Icon(FontAwesomeIcons.mapMarkerAlt),
-        optional: false,
-        onChanged: (value) => publicTransportModel.departureLocation = value,
-      ),
-      DateFormField(
-        key: _depDateFormFieldKey,
-        labelText: 'Departure Date *',
-        icon: Icon(FontAwesomeIcons.calendarAlt),
-        chosenDateString: (value) => publicTransportModel.departureDate = value,
-      ),
-      TimeFormField(
-        labelText: 'Departure Time *',
-        icon: Icon(FontAwesomeIcons.clock),
-        chosenTimeString: (value) => publicTransportModel.departureTime = value,
-      ),
-      SectionTitle('Arrival Information'),
-      TravelloryFormField(
-        labelText: 'Arrival Location *',
-        icon: Icon(FontAwesomeIcons.mapMarkerAlt),
-        optional: false,
-        onChanged: (value) => publicTransportModel.arrivalLocation = value,
-      ),
-      DateFormField(
-        labelText: 'Arrival Date *',
-        icon: Icon(FontAwesomeIcons.calendarAlt),
-        beforeDateKey: _depDateFormFieldKey,
-        dateValidationMessage: 'Departure Date cannot be before Arrival Date',
-        chosenDateString: (value) => publicTransportModel.arrivalDate = value,
-      ),
-      TimeFormField(
-        labelText: 'Arrival Time',
-        icon: Icon(FontAwesomeIcons.clock),
-        optional: true,
-        chosenTimeString: (value) => publicTransportModel.arrivalTime = value,
-      ),
-      SectionTitle('Booking Details'),
-      bookingMadeCheckbox,
-      seatReservedCheckbox,
-      SectionTitle('Notes'),
-      TravelloryFormField(
-        labelText: 'Notes',
-        icon: Icon(FontAwesomeIcons.stickyNote),
-        optional: true,
-        onChanged: (value) => publicTransportModel.notes = value,
-      ),
-      SubmitButton(),
-      CancelButton(),
-      SizedBox(height: 20),
-    ];
-
-    // this builds the animated list
-    publicTransportList = ListModel<Widget>(
-      listKey: _listKey,
-      initialItems: shown,
-      removedItemBuilder: _removedItemBuilder,
-    );
-
-    typeSpecificationAdditional = Column(
-      children: <Widget>[
-        TravelloryFormField(
-          labelText: 'Specific Type of Transportation',
-          icon: Icon(FontAwesomeIcons.train),
-          optional: true,
-          onChanged: (value) => publicTransportModel.specificType = value,
-        ),
-      ],
-    );
-
-    bookingMadeAdditional = Column(
-      children: <Widget>[
-        TravelloryFormField(
-          labelText: 'Booking Reference',
-          icon: Icon(FontAwesomeIcons.ticketAlt),
-          optional: true,
-          onChanged: (value) => publicTransportModel.referenceNr = value,
-        ),
-        TravelloryFormField(
-          labelText: 'Booking Company',
-          icon: Icon(FontAwesomeIcons.building),
-          optional: true,
-          onChanged: (value) => publicTransportModel.reservationCompany = value,
-        ),
-      ],
-    );
-
-    seatReservationAdditional = Column(
-      children: <Widget>[
-        TravelloryFormField(
-          labelText: 'Seat',
-          icon: Icon(FontAwesomeIcons.chair),
-          optional: true,
-          onChanged: (value) => publicTransportModel.seat = value,
-        ),
-      ],
-    );
   }
 
   final String alertText =
@@ -201,27 +59,229 @@ class PublicTransportState<T extends PublicTransport> extends State<T> {
     return FormItem(animation: animation, child: item);
   }
 
-  @override
-  Widget build(BuildContext context) {
-    final SingleTripProvider singleTripProvider =
-        Provider.of<TripsProvider>(context, listen: false).selectedTrip;
-    final TripModel tripModel = singleTripProvider.tripModel;
-    publicTransportModel.tripUID = tripModel.uid;
-
-    // replace widget to get the context
-    publicTransportList[publicTransportList.length - 3] = SubmitButton(
-      highlightColor: Theme.of(context).primaryColor,
-      fillColor: Theme.of(context).primaryColor,
-      validationFunction: validateForm,
-      onSubmit: onSubmitBooking(singleTripProvider, publicTransportModel,
-          'booking-addPublicTransportation', context, alertText),
-    );
-
+  void _getCancelButton(BuildContext context) {
     publicTransportList[publicTransportList.length - 2] = CancelButton(
       text: 'CANCEL',
       onCancel: () {
         cancellingDialog(context, cancelText);
       },
+    );
+  }
+
+  void _getSubmitButton(SingleTripProvider singleTripProvider, BuildContext context,
+      PublicTransportModel model, bool isNewModel) {
+    void Function() onSubmit;
+    if (isNewModel) {
+      onSubmit = onSubmitBooking(
+          singleTripProvider, model, 'booking-addPublicTransportation', context, alertText);
+    } else {
+      onSubmit = onEditBooking(singleTripProvider, model, context, errorMessage);
+    }
+
+    publicTransportList[publicTransportList.length - 3] = SubmitButton(
+      highlightColor: Theme.of(context).primaryColor,
+      fillColor: Theme.of(context).primaryColor,
+      validationFunction: validateForm,
+      onSubmit: onSubmit,
+    );
+  }
+
+  Column getContent(TripModel tripModel, SingleTripProvider singleTripProvider,
+      BuildContext context, PublicTransportModel model, bool isNewModel) {
+    _getSubmitButton(singleTripProvider, context, model, isNewModel);
+    _getCancelButton(context);
+
+    return Column(
+      children: <Widget>[
+        TripHeader(tripModel),
+        Expanded(
+            child: Container(
+          height: double.infinity,
+          child: Form(
+            key: publicTransportFormKey,
+            child: Column(
+              children: <Widget>[
+                Expanded(
+                  child: AnimatedList(
+                    key: _listKey,
+                    initialItemCount: publicTransportList.length,
+                    itemBuilder: itemBuilder,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        )),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final SingleTripProvider singleTripProvider =
+        Provider.of<TripsProvider>(context, listen: false).selectedTrip;
+    final TripModel tripModel = singleTripProvider.tripModel;
+
+    ModifyModelArguments arguments = ModalRoute.of(context).settings.arguments;
+    PublicTransportModel _publicTransportModel = arguments.model;
+
+    TravelloryDropdownField transportTypeDropdown;
+    CheckboxFormField bookingMadeCheckbox;
+    CheckboxFormField seatReservedCheckbox;
+
+    Widget typeSpecificationAdditional;
+    Widget bookingMadeAdditional;
+    Widget seatReservationAdditional;
+
+    transportTypeDropdown = TravelloryDropdownField(
+        initialValue: _publicTransportModel.transportationType,
+        title: 'Select Transport Type',
+        types: publicTransportTypes,
+        onChanged: (value) {
+          _publicTransportModel.transportationType = value.name;
+          showAdditional(publicTransportList, value.name == 'Other', transportTypeDropdown,
+              typeSpecificationAdditional);
+        },
+        validatorText: 'Please enter the required information');
+
+    bookingMadeCheckbox = CheckboxFormField(
+      initialValue: _publicTransportModel.booked,
+      label: 'Did you book this public transport?',
+      onChanged: (value) {
+        _publicTransportModel.booked = value;
+        showAdditional(publicTransportList, value, bookingMadeCheckbox, bookingMadeAdditional);
+      },
+    );
+
+    seatReservedCheckbox = CheckboxFormField(
+        initialValue: _publicTransportModel.seatReserved,
+        label: 'Did you make a seat reservation?',
+        onChanged: (value) {
+          _publicTransportModel.seatReserved = value;
+          showAdditional(
+              publicTransportList, value, seatReservedCheckbox, seatReservationAdditional);
+        });
+
+    // don't put in build because it will be recreated on every build
+    // with state changes this is not appreciated
+    final List<Widget> shown = [
+      BookingSiteTitle('Add Public Transport', FontAwesomeIcons.train),
+      SectionTitle('Type of Transportation'),
+      transportTypeDropdown,
+      TravelloryFormField(
+          initialValue: _publicTransportModel.publicTransportCompany,
+          labelText: 'Company',
+          icon: Icon(FontAwesomeIcons.solidBuilding),
+          optional: true,
+          onChanged: (value) => _publicTransportModel.publicTransportCompany = value),
+      SectionTitle('Departure Information'),
+      TravelloryFormField(
+        initialValue: _publicTransportModel.departureLocation,
+        labelText: 'Departure Location *',
+        icon: Icon(FontAwesomeIcons.mapMarkerAlt),
+        optional: false,
+        onChanged: (value) => _publicTransportModel.departureLocation = value,
+      ),
+      DateFormField(
+        initialValue: _publicTransportModel.departureDate,
+        key: _depDateFormFieldKey,
+        labelText: 'Departure Date *',
+        icon: Icon(FontAwesomeIcons.calendarAlt),
+        chosenDateString: (value) => _publicTransportModel.departureDate = value,
+      ),
+      TimeFormField(
+        initialValue: _publicTransportModel.departureTime,
+        labelText: 'Departure Time *',
+        icon: Icon(FontAwesomeIcons.clock),
+        chosenTimeString: (value) => _publicTransportModel.departureTime = value,
+      ),
+      SectionTitle('Arrival Information'),
+      TravelloryFormField(
+        initialValue: _publicTransportModel.arrivalLocation,
+        labelText: 'Arrival Location *',
+        icon: Icon(FontAwesomeIcons.mapMarkerAlt),
+        optional: false,
+        onChanged: (value) => _publicTransportModel.arrivalLocation = value,
+      ),
+      DateFormField(
+        initialValue: _publicTransportModel.arrivalDate,
+        labelText: 'Arrival Date *',
+        icon: Icon(FontAwesomeIcons.calendarAlt),
+        beforeDateKey: _depDateFormFieldKey,
+        dateValidationMessage: 'Departure Date cannot be before Arrival Date',
+        chosenDateString: (value) => _publicTransportModel.arrivalDate = value,
+      ),
+      TimeFormField(
+        initialValue: _publicTransportModel.arrivalTime,
+        labelText: 'Arrival Time',
+        icon: Icon(FontAwesomeIcons.clock),
+        optional: true,
+        chosenTimeString: (value) => _publicTransportModel.arrivalTime = value,
+      ),
+      SectionTitle('Booking Details'),
+      bookingMadeCheckbox,
+      seatReservedCheckbox,
+      SectionTitle('Notes'),
+      TravelloryFormField(
+        initialValue: _publicTransportModel.notes,
+        labelText: 'Notes',
+        icon: Icon(FontAwesomeIcons.stickyNote),
+        optional: true,
+        onChanged: (value) => _publicTransportModel.notes = value,
+      ),
+      SubmitButton(),
+      CancelButton(),
+      SizedBox(height: 20),
+    ];
+
+    // this builds the animated list
+    publicTransportList = ListModel<Widget>(
+      listKey: _listKey,
+      initialItems: shown,
+      removedItemBuilder: _removedItemBuilder,
+    );
+
+    typeSpecificationAdditional = Column(
+      children: <Widget>[
+        TravelloryFormField(
+          initialValue: _publicTransportModel.specificType,
+          labelText: 'Specific Type of Transportation',
+          icon: Icon(FontAwesomeIcons.train),
+          optional: true,
+          onChanged: (value) => _publicTransportModel.specificType = value,
+        ),
+      ],
+    );
+
+    bookingMadeAdditional = Column(
+      children: <Widget>[
+        TravelloryFormField(
+          initialValue: _publicTransportModel.referenceNr,
+          labelText: 'Booking Reference',
+          icon: Icon(FontAwesomeIcons.ticketAlt),
+          optional: true,
+          onChanged: (value) => _publicTransportModel.referenceNr = value,
+        ),
+        TravelloryFormField(
+          initialValue: _publicTransportModel.reservationCompany,
+          labelText: 'Booking Company',
+          icon: Icon(FontAwesomeIcons.building),
+          optional: true,
+          onChanged: (value) => _publicTransportModel.reservationCompany = value,
+        ),
+      ],
+    );
+
+    seatReservationAdditional = Column(
+      children: <Widget>[
+        TravelloryFormField(
+          initialValue: _publicTransportModel.seat,
+          labelText: 'Seat',
+          icon: Icon(FontAwesomeIcons.chair),
+          optional: true,
+          onChanged: (value) => _publicTransportModel.seat = value,
+        ),
+      ],
     );
 
     return Scaffold(
@@ -229,29 +289,8 @@ class PublicTransportState<T extends PublicTransport> extends State<T> {
       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       body: Container(
         color: Colors.white,
-        child: Column(
-          children: <Widget>[
-            TripHeader(tripModel),
-            Expanded(
-                child: Container(
-              height: double.infinity,
-              child: Form(
-                key: publicTransportFormKey,
-                child: Column(
-                  children: <Widget>[
-                    Expanded(
-                      child: AnimatedList(
-                        key: _listKey,
-                        initialItemCount: publicTransportList.length,
-                        itemBuilder: itemBuilder,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            )),
-          ],
-        ),
+        child: getContent(
+            tripModel, singleTripProvider, context, _publicTransportModel, arguments.isNewModel),
       ),
     );
   }
