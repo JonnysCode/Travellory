93b7cf1e81be8da647f8ae4723e633116adbf185
antilyas <antilyas@students.zhaw.ch>
2020-05-02 17:33:40 +0200

    * refactored getContent build for edit accommodation

--- lib/screens/bookings/edit_accommodation.dart
+++ lib/screens/bookings/edit_accommodation.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:travellory/models/accommodation_model.dart';
-import 'package:travellory/models/activity_model.dart';
 import 'package:travellory/models/trip_model.dart';
 import 'package:travellory/providers/single_trip_provider.dart';
 import 'package:travellory/providers/trips_provider.dart';
 import 'package:travellory/screens/bookings/add_accommodation.dart';
+import 'package:travellory/services/database/submit.dart';
+import 'package:travellory/widgets/buttons/buttons.dart';
+import 'package:travellory/widgets/forms/show_dialog.dart';
 
 class EditAccommodation extends Accommodation {
   EditAccommodation({
@@ -17,6 +19,24 @@ class EditAccommodation extends Accommodation {
 }
 
 class _EditAccommodationState extends AccommodationState<EditAccommodation> {
+
+//  @override
+//  Widget build(BuildContext context) {
+//    final SingleTripProvider singleTripProvider =
+//        Provider.of<TripsProvider>(context, listen: false).selectedTrip;
+//    final TripModel tripModel = singleTripProvider.tripModel;
+//
+//    final AccommodationModel _accommodationModel = ModalRoute.of(context).settings.arguments;
+//
+//    return Scaffold(
+//      key: Key('EditAccommodation'),
+//      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+//      body: Container(
+//        color: Colors.white,
+////        child: getContent(context, singleTripProvider, tripModel, _accommodationModel, false),
+//      ),
+//    );
+//  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,7 +51,7 @@ class _EditAccommodationState extends AccommodationState<EditAccommodation> {
       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       body: Container(
         color: Colors.white,
-//        child: getContent(context, singleTripProvider, tripModel, _accommodationModel, false),
+        child: getContent(tripModel, singleTripProvider, context),
       ),
     );
   }
