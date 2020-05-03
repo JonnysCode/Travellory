46e36e5069d58c70c88431842515f55088c4fc62
grussjon <grussjon@students.zhaw.ch>
2020-05-03 15:08:45 +0200

    * LoadingLogo when fetching bookings

--- lib/screens/home/pages/home_page.dart
+++ lib/screens/home/pages/home_page.dart
@@ -2,19 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
 import 'package:travellory/models/trip_model.dart';
+import 'package:travellory/providers/single_trip_provider.dart';
 import 'package:travellory/providers/trips_provider.dart';
 import 'package:travellory/screens/trip/schedule/trip_schedule.dart';
 import 'package:travellory/shared/loading_heart.dart';
 import 'package:travellory/widgets/buttons/speed_dial_button.dart';
 import 'package:travellory/widgets/font_widgets.dart';
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-
+class HomePage extends StatelessWidget {
   static const List<Dial> _dials = <Dial>[
     Dial(
       icon: FontAwesomeIcons.envelope,
@@ -49,109 +44,101 @@ class _HomePageState extends State<HomePage> {
   ];
 
   @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
+    SingleTripProvider trip = Provider.of<TripsProvider>(context, listen: false).activeTrip;
     return SafeArea(
-      child: Consumer<TripsProvider>(
-        builder: (_, trips, __) => !trips.activeTripInitiated
-            ? LoadingHeart()
-            : Container(
-          key: Key('home_page'),
-          child: Stack(
-            children: <Widget>[
-              Positioned(
-                left: 30,
-                top: 30,
-                child: Image(
-                  height: 100,
-                  image: AssetImage('assets/images/home/011-cloud.png'),
+      child: Container(
+        key: Key('home_page'),
+        child: Stack(
+          children: <Widget>[
+            Positioned(
+              left: 30,
+              top: 30,
+              child: Image(
+                height: 100,
+                image: AssetImage('assets/images/home/011-cloud.png'),
+              ),
+            ),
+            Positioned(
+              top: 100,
+              left: 110,
+              child: FashionFetishText(
+                text: '24\u00B0',
+                size: 24,
+                color: Colors.black87,
+                fontWeight: FashionFontWeight.bold,
+              ),
+            ),
+            Positioned(
+              top: 40,
+              left: 175,
+              right: 20,
+              child: FashionFetishText(
+                text: 'Get ready Bill!',
+                size: 24,
+                fontWeight: FashionFontWeight.heavy,
+                height: 1.2,
+              ),
+            ),
+            Positioned(
+              top: 75,
+              left: 175,
+              right: 40,
+              child: Text(
+                'Your trip to Los Angeles starts in 1 day. Pack your bags now.',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.black54,
                 ),
               ),
-              Positioned(
-                top: 100,
-                left: 110,
-                child: FashionFetishText(
-                  text: '24\u00B0',
-                  size: 24,
-                  color: Colors.black87,
-                  fontWeight: FashionFontWeight.bold,
-                ),
-              ),
-              Positioned(
-                top: 40,
-                left: 175,
-                right: 20,
-                child: FashionFetishText(
-                  text: 'Get ready Bill!',
-                  size: 24,
-                  fontWeight: FashionFontWeight.heavy,
-                  height: 1.2,
-                ),
-              ),
-              Positioned(
-                top: 75,
-                left: 175,
-                right: 40,
-                child: Text(
-                  'Your trip to Los Angeles starts in 1 day. Pack your bags now.',
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.black54,
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 6.0),
+                child: Container(
+                  height: MediaQuery.of(context).size.height*0.8,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.vertical(top: Radius.circular(40.0)),
+                    boxShadow: <BoxShadow>[
+                      BoxShadow(blurRadius: 18, color: Colors.black.withOpacity(.2), offset: Offset(0.0, -6.0))
+                    ],
                   ),
-                ),
-              ),
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 6.0),
-                  child: Container(
-                    height: MediaQuery.of(context).size.height*0.8,
-                    width: MediaQuery.of(context).size.width,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.vertical(top: Radius.circular(40.0)),
-                      boxShadow: <BoxShadow>[
-                        BoxShadow(blurRadius: 18, color: Colors.black.withOpacity(.2), offset: Offset(0.0, -6.0))
+                  child:  Column(
+                      children: <Widget>[
+                        FashionFetishText(
+                          text: trip.tripModel.name,
+                          size: 20,
+                          height: 1.6,
+                          fontWeight: FashionFontWeight.heavy,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 3),
+                          child: Container(
+                            height: 1,
+                            color: Colors.black12,
+                          ),
+                        ),
+                        Expanded(
+                          child: Schedule(
+                            key: Key('home_schedule'),
+                            trip: trip,
+                          ),
+                        ),
                       ],
                     ),
-                    child:  Column(
-                        children: <Widget>[
-                          FashionFetishText(
-                            text: trips.activeTrip.tripModel.name,
-                            size: 20,
-                            height: 1.6,
-                            fontWeight: FashionFontWeight.heavy,
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 3),
-                            child: Container(
-                              height: 1,
-                              color: Colors.black12,
-                            ),
-                          ),
-                          Expanded(
-                            child: Schedule(
-                              key: Key('home_schedule'),
-                              trip: trips.activeTrip,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
                   ),
                 ),
-              SpeedDialButton(
-                key: Key('home_page_dial'),
-                dials: _dials,
-                tripModel: trips.activeTrip.tripModel,
               ),
-            ],
-          ),
-      ),
+            SpeedDialButton(
+              key: Key('home_page_dial'),
+              dials: _dials,
+              tripModel: trip.tripModel,
+            ),
+          ],
+        ),
       ),
     );
   }
