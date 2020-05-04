8c457bec73263c0b846fb2e12be4948460f8df20
Dávid Isztl <isztl.david@gmail.com>
2020-05-04 22:11:35 +0200

    cleanup and tests added

--- lib/utils/g_map/g_map_border_loader.dart
+++ lib/utils/g_map/g_map_border_loader.dart
@@ -8,52 +8,6 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 class GMapBorderLoader{
   static String data;
 
-  /*
-  @visibleForTesting
-  static Future<List<LatLng>> doPoints(String stateName) async {
-    final List<LatLng> points = <LatLng>[];
-
-    try {
-      data = await rootBundle.loadString('assets/g_map/border_points/${stateName.toLowerCase()}.json');
-    }catch(_){
-      return points;
-    }
-
-    final jsonResult = await json.decode(data);
-    final List results = jsonResult["geometry"] as List;
-    for(final result in results) {
-      points.add(LatLng(result[0].toDouble(),result[1].toDouble()));
-    }
-
-    return points;
-  }
-
-  @visibleForTesting
-  static Future<Polygon> doPolygon(String stateName) async {
-    final points = await doPoints(stateName);
-    return Polygon(
-      polygonId: PolygonId(stateName),
-      consumeTapEvents: false,
-      fillColor: Color.fromRGBO(255, 0, 0, 0.35),
-      geodesic: false,
-      points: points,
-      strokeColor: Color.fromRGBO(255, 0, 0, 0.8),
-      strokeWidth: 2,
-      visible: true,
-      zIndex: 0,
-      //onTap: (){print("Tapped!");},
-    );
-  }
-*/
-
-
-
-
-  ////////////////// OLDDDDD ABOVE!!! //////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-
   static void readMultiPolygon(List multiPolygon, List<Polygon> return_polygons, String stateName) async {
 
     for(int i=0; i<multiPolygon.length; i++){
@@ -61,12 +15,6 @@ class GMapBorderLoader{
       for(int j=0; j<(multiPolygon[i][0] as List).length; j++){
         points.add(LatLng(multiPolygon[i][0][j][1].toDouble(),multiPolygon[i][0][j][0].toDouble()));
       }
-      /*
-      for(final List dots in multiPolygon[i]) {
-        points.add(LatLng(dots[1].toDouble(),dots[0].toDouble()));
-      }
-
-       */
       return_polygons.add(Polygon(
         polygonId: PolygonId(stateName+"_"+i.toString()),
         consumeTapEvents: false,
@@ -80,28 +28,6 @@ class GMapBorderLoader{
         //onTap: (){print("Tapped!");},
       ));
     }
-
-    /*
-    for(final dynamic polygon in multiPolygon) {
-      final List<LatLng> points = <LatLng>[];
-      for(final dynamic dots in polygon[0]) {
-        points.add(LatLng(dots[1].toDouble(),dots[0].toDouble()));
-      }
-      return_polygons.add(Polygon(
-        polygonId: PolygonId(stateName),
-        consumeTapEvents: false,
-        fillColor: Color.fromRGBO(255, 0, 0, 0.35),
-        geodesic: false,
-        points: points,
-        strokeColor: Color.fromRGBO(255, 0, 0, 0.8),
-        strokeWidth: 2,
-        visible: true,
-        zIndex: 0,
-        //onTap: (){print("Tapped!");},
-      ));
-    }
-
-     */
   }
 
   static void readPolygon(List polygon, List<Polygon> return_polygons, String stateName) async {
@@ -136,9 +62,6 @@ class GMapBorderLoader{
     }
 
     final Map jsonResult = await json.decode(data);
-    //print(stateName);
-    //print(jsonResult["features"][0]["geometry"]["coordinates"][0] as List);
-
 
     switch (jsonResult["features"][0]["geometry"]["type"]){
       case "Polygon":
@@ -152,26 +75,13 @@ class GMapBorderLoader{
         break;
     }
 
-
-
     return polygons;
   }
-
 
 
   static Future<List<Polygon>> generateBorders (List<String> states) async {
 
     final List<Polygon> borders = <Polygon>[];
-
-/*
-    for(final stateName in states) {
-      final Polygon state = await doPolygon(stateName);
-      if(state.points.isNotEmpty){
-        borders.add(state);
-      }
-    }
-    return borders;
-    */
 
     for(final stateName in states) {
       final List<Polygon> state = await fetchPolygons(stateName);
