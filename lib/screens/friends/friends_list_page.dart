413e6e8dc9d51777f35b2fbb900f5f395cb08fc9
unknown <fluetfab@students.zhaw.ch>
2020-05-05 23:47:57 +0200

    make lists in friends-page expandable

--- lib/screens/friends/friends_list_page.dart
+++ lib/screens/friends/friends_list_page.dart
@@ -168,51 +168,47 @@ class _FriendListPageState extends State<FriendListPage> {
             ),
           ),
         ),
-        Padding(
-          key: Key('friend-requests-list'),
-          padding: EdgeInsets.only(
-              left: 15,
-              right: 20,
-              bottom: MediaQuery.of(context).viewInsets.bottom),
-          child: Flex(
-              direction: Axis.horizontal,
-              children: <Widget>[
-                Expanded(
-//                  flex: 2,
-                  child: Scrollbar(
-                      child: Consumer<FriendsProvider>(
-                        builder: (_, friendsProvider, __) =>
-                        friendsProvider.isFetching
-                            ? LoadingHeart()
-                            : friendsProvider.friendRequests.isEmpty
-                            ? Text('No pending friend requests')
-                            : ListView.separated(
-                          padding: EdgeInsets.only(
-                            bottom: 50,
+        Expanded(
+          flex: 3,
+          child: Padding(
+            key: Key('friend-requests-list'),
+            padding: EdgeInsets.only(
+                left: 15,
+                right: 20,
+                bottom: MediaQuery.of(context).viewInsets.bottom),
+                child: Scrollbar(
+                    child: Consumer<FriendsProvider>(
+                      builder: (_, friendsProvider, __) =>
+                      friendsProvider.isFetching
+                          ? LoadingHeart()
+                          : friendsProvider.friendRequests.isEmpty
+                          ? Text('No pending friend requests')
+                          : ListView.separated(
+                              padding: EdgeInsets.only(
+                                bottom: 50,
+                              ),
+                              scrollDirection: Axis.vertical,
+                              shrinkWrap: true,
+                              separatorBuilder: (context, index) =>
+                              const SizedBox(height: 12),
+                              itemCount: friendsProvider.friendRequests.length,
+                              itemBuilder: (context, index) {
+                                FriendsModel friend =
+                                friendsProvider.friendRequests[index];
+                                return friendsCard(
+                                  context,
+                                  friend,
+                                  _loading
+                                      ? CircularProgressIndicator()
+                                      : friendRequestButtons(
+                                      friend.uid, user.uid),
+                                  10,
+                                );
+                              },
                           ),
-                          scrollDirection: Axis.vertical,
-                          shrinkWrap: true,
-                          separatorBuilder: (context, index) =>
-                          const SizedBox(height: 12),
-                          itemCount: friendsProvider.friendRequests.length,
-                          itemBuilder: (context, index) {
-                            FriendsModel friend =
-                            friendsProvider.friendRequests[index];
-                            return friendsCard(
-                              context,
-                              friend,
-                              _loading
-                                  ? CircularProgressIndicator()
-                                  : friendRequestButtons(
-                                  friend.uid, user.uid),
-                              10,
-                            );
-                          },
-                        ),
-                      )
-                  ),
+                    )
                 ),
-          ]),
+          ),
         ),
         SizedBox(height: 20),
         Padding(
@@ -232,29 +228,25 @@ class _FriendListPageState extends State<FriendListPage> {
             ),
           ),
         ),
-        Padding(
-          key: Key('friends-list'),
-          padding: EdgeInsets.only(
-              left: 15,
-              right: 20,
-              bottom: MediaQuery.of(context).viewInsets.bottom),
-          child: Flex(
-              direction: Axis.horizontal,
-              children: <Widget>[
-                Expanded(
-//                  flex: 2,
-//              height: 0.38 * MediaQuery.of(context).size.height,
-                  child: Scrollbar(
-                      child: Consumer<FriendsProvider>(
-                        builder: (_, friendsProvider, __) =>
-                        friendsProvider.isFetching
-                            ? LoadingHeart()
-                            : friendsProvider.friends.isEmpty
-                            ? Text('You have no friends :(')
-                            : ListView.separated(
-//                          padding: EdgeInsets.only(
-//                            bottom: 50,
-//                          ),
+        Expanded(
+          flex: 7,
+          child: Padding(
+            key: Key('friends-list'),
+            padding: EdgeInsets.only(
+                left: 15,
+                right: 20,
+                bottom: MediaQuery.of(context).viewInsets.bottom),
+            child: Scrollbar(
+                child: Consumer<FriendsProvider>(
+                  builder: (_, friendsProvider, __) =>
+                  friendsProvider.isFetching
+                      ? LoadingHeart()
+                      : friendsProvider.friends.isEmpty
+                      ? Text('You have no friends :(')
+                      : ListView.separated(
+                          padding: EdgeInsets.only(
+                            bottom: 50,
+                          ),
                           scrollDirection: Axis.vertical,
                           shrinkWrap: true,
                           separatorBuilder: (context, index) =>
@@ -273,12 +265,12 @@ class _FriendListPageState extends State<FriendListPage> {
                               10,
                             );
                           },
-                        ),
-                      )
-                  ),
-                ),
-          ]),
+                      ),
+                )
+            ),
+          ),
         ),
+        SizedBox(height: 50),
       ],
     );
   }
