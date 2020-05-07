5eb2ffc9c40390f6edfbe1c0b4f6a68f90288fce
unknown <fluetfab@students.zhaw.ch>
2020-05-08 00:02:43 +0200

    add layout to friends-profile

--- lib/widgets/friends/friends_profile.dart
+++ lib/widgets/friends/friends_profile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:travellory/models/friends_model.dart';
+import '../font_widgets.dart';
 import 'friends_profile_header.dart';
 
 class FriendsProfile extends StatefulWidget {
@@ -23,6 +24,43 @@ class _FriendsProfileState extends State<FriendsProfile> {
         child: Column(
           children: <Widget>[
             FriendsProfileHeader(friend: friend),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              child: Stack(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: 30,
+                      left: 20,
+                    ),
+                    child: FashionFetishText(
+                      text: 'Trips of ${friend.username}' ,
+                      size: 22,
+                      fontWeight: FashionFontWeight.heavy,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 300),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              child: Stack(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(
+                      top: 30,
+                      left: 20,
+                    ),
+                    child: FashionFetishText(
+                      text: 'Achievements of ${friend.username}',
+                      size: 22,
+                      fontWeight: FashionFontWeight.heavy,
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
