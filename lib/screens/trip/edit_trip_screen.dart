47b78902535ce89fd0aaee498e812365da182aa1
grussjon <grussjon@students.zhaw.ch>
2020-05-15 10:37:02 +0200

    * edit trip refactoring

--- lib/screens/trip/edit_trip_screen.dart
+++ lib/screens/trip/edit_trip_screen.dart
@@ -1,19 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:travellory/widgets/trip/trip_form.dart';
 
-class CreateTrip extends StatelessWidget {
-  static final route = '/createtrip';
+class EditTrip extends StatelessWidget {
+  static final route = '/edittrip';
 
   @override
   Widget build(BuildContext context) {
 
     return SafeArea(
       child: Scaffold(
-        key: Key('create_trip'),
+        key: Key('edit_trip'),
         backgroundColor: Theme.of(context).primaryColor,
         body: Padding(
           padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 8),
-          child: TripForm.create(),
+          child: TripForm.edit(),
         ),
       ),
     );
