3fe2a7316ffcbfc2aa77121711c91065c9adf647
grussjon <jonathangruss@hotmail.com>
2020-05-17 18:51:58 +0200

    + ImageSelector as Widget

--- lib/screens/trip/create_trip_screen.dart
+++ lib/screens/trip/create_trip_screen.dart
@@ -9,6 +9,7 @@ import 'package:travellory/widgets/bookings/bookings_get_buttons.dart';
 import 'package:travellory/widgets/bookings/edit.dart';
 import 'package:travellory/widgets/forms/date_form_field.dart';
 import 'package:travellory/widgets/forms/form_field.dart';
+import 'package:travellory/widgets/forms/image_selector.dart';
 import 'package:travellory/widgets/forms/section_titles.dart';
 import 'package:travellory/widgets/forms/show_dialog.dart';
 import 'package:travellory/services/api/google_places.dart';
@@ -38,12 +39,6 @@ class _CreateTripState extends State<CreateTrip> {
       'Do you want to go back to the previous site and discard your changes?';
 
   TripModel _tripModel;
-  int _selectedIndex;
-
-  @override
-  void initState() {
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -52,13 +47,6 @@ class _CreateTripState extends State<CreateTrip> {
 
     _tripModel = TripModel.fromData(arguments.model.toMap());
     final bool isNewModel = arguments.isNewModel;
-
-    if (!isNewModel) {
-      _selectedIndex = _tripModel.imageNr - 1;
-    } else {
-      _selectedIndex = 0;
-      _tripModel.imageNr = 1;
-    }
 
     bool _validateForm() {
       return createTripFormKey.currentState.validate();
@@ -184,12 +172,15 @@ class _CreateTripState extends State<CreateTrip> {
                           labelText: 'Trip Title *',
                           icon: Icon(Icons.supervised_user_circle),
                           optional: false,
-                          onChanged: (value) => {print(_tripModel.name = value)},
+                          onChanged: (value) => _tripModel.name = value,
                         ),
                       ),
                       Padding(
                         padding: const EdgeInsets.only(top: 10, left: 15, right: 15),
-                        child: _imageSelection(),
+                        child: ImageSelector(
+                          initialValue: _tripModel.imageNr,
+                          onChanged: (value) => _tripModel.imageNr = value,
+                        ),
                       ),
                       SizedBox(
                         height: 12,
@@ -219,63 +210,6 @@ class _CreateTripState extends State<CreateTrip> {
                     ]),
                   ),
                 ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _imageSelection() {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Container(
-        height: 96,
-        child: ListView.separated(
-          scrollDirection: Axis.horizontal,
-          shrinkWrap: true,
-          itemCount: _imageItemCount,
-          itemBuilder: (context, index) {
-            return _imageItem(index);
-          },
-          separatorBuilder: (context, index) => const SizedBox(),
-        ),
-      ),
-    );
-  }
-
-  void _selectImage(index) {
-    setState(() {
-      _selectedIndex = index;
-      _tripModel.imageNr = _selectedIndex + 1;
-    });
-  }
-
-  Widget _imageItem(int index) {
-    return Center(
-      child: GestureDetector(
-        onTap: () => _selectImage(index),
-        child: AnimatedContainer(
-          duration: Duration(milliseconds: 200),
-          height: _selectedIndex == index ? 80 : 72,
-          width: _selectedIndex == index ? 80 : 72,
-          padding: _selectedIndex == index ? const EdgeInsets.all(8.0) : const EdgeInsets.all(3.0),
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(40.0),
-            color: _selectedIndex == index ? Colors.black26 : Colors.transparent,
-          ),
-          child: Container(
-            key: Key('image_icon'),
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/images/home/trip/trip_${(index + 1).toString()}.png'),
-                fit: BoxFit.fitWidth,
-              ),
-              borderRadius: BorderRadius.circular(33.0),
-              boxShadow: <BoxShadow>[
-                BoxShadow(
-                    blurRadius: 4, color: Colors.black.withOpacity(.25), offset: Offset(2.0, 2.0))
               ],
             ),
           ),
