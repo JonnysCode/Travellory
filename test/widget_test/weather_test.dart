169c73c63f3530bd0f4971da15ed67ff7137de9a
schinsev <schinsev@students.zhaw.ch>
2020-05-18 20:10:14 +0200

    weather thing

--- test/widget_test/weather_test.dart
+++ test/widget_test/weather_test.dart
@@ -7,7 +7,7 @@ import 'package:travellory/utils/weather.dart';
 class MockWeather extends Mock implements OpenWeatherAPI {}
 
 void main() {
-  Widget makeTestableWidget({Widget child}) {
+  Widget makeTestableWidget({Widget child, OpenWeatherAPI weatherAPI}) {
     return MaterialApp(
       home: child,
     );
@@ -15,9 +15,10 @@ void main() {
 
   testWidgets('test if page is the weather page', (WidgetTester tester) async {
     Weather page = Weather('Winterthur');
+    MockWeather mockWeather = MockWeather();
 
     // Build our app and trigger a frame.
-    await tester.pumpWidget(await makeTestableWidget(child: page));
+    await tester.pumpWidget(await makeTestableWidget(child: page,weatherAPI: mockWeather));
 
     // Verify that the weather page is present.
     expect(find.byKey(Key('weather_page')), findsOneWidget);
