32ffd1540484d571b02aa67e2ef4f44adc3cd6ee
grussjon <grussjon@students.zhaw.ch>
2020-05-19 18:55:41 +0200

    * removed duplicate code

--- lib/screens/authenticate/password.dart
+++ lib/screens/authenticate/password.dart
@@ -4,6 +4,7 @@ import 'package:travellory/providers/auth_provider.dart';
 import 'package:travellory/services/authentication/auth.dart';
 import 'package:travellory/shared/loading_logo.dart';
 import 'package:travellory/utils/input_validator.dart';
+import 'package:travellory/widgets/auth_background.dart';
 import 'package:travellory/widgets/buttons/buttons.dart';
 import 'package:travellory/widgets/input_widgets.dart';
 import 'package:flushbar/flushbar.dart';
@@ -18,7 +19,6 @@ class ChangePassword extends StatefulWidget {
 
 class _RegisterState extends State<ChangePassword> {
   final _formKey = GlobalKey<FormState>();
-  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   final String alertText = "You've successfully changed your password.";
 
   final TextEditingController _oldPasswordController = TextEditingController();
@@ -78,149 +78,80 @@ class _RegisterState extends State<ChangePassword> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      key: _scaffoldKey,
       backgroundColor: Theme.of(context).primaryColor,
-      body: Column(
-        children: <Widget>[
-          SizedBox(
-              height: MediaQuery.of(context).size.height * 0.05
-          ),
-          DecoratedBox(
-            decoration: BoxDecoration(
-              color: Theme.of(context).canvasColor,
-            ),
-            child: ClipRRect(
-              borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(40.0),
-                  topRight: Radius.circular(40.0)
+      body: AuthBackground(
+        hasExitButton: true,
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.only(top: 20, bottom: 8),
+              child: Text(
+                'Please first enter the old password',
+                textAlign: TextAlign.center,
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(
+                    fontSize: 16
+                ),
               ),
-              child: Container(
-                height: MediaQuery.of(context).size.height * 0.95,
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                      image: AssetImage("assets/images/login/beach.png"),
-                      fit: BoxFit.fitWidth,
-                      alignment: Alignment.bottomCenter
+            ),
+            Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 20),
+                    child: inputAuthentication(
+                        Icon(FontAwesomeIcons.unlockAlt),
+                        "OLD PASSWORD",
+                        Theme.of(context).primaryColor,
+                        _oldPasswordController,
+                        null,
+                        ValidatorType.password,
+                        true,
+                        _reauthError
+                    ),
                   ),
-                ),
-                child: ListView(
-                  children: <Widget>[
-                    Container(
+                  Padding(
+                    padding: EdgeInsets.only(bottom: 20),
+                    child: inputAuthentication(
+                        Icon(FontAwesomeIcons.unlockAlt),
+                        "NEW PASSWORD",
+                        Theme.of(context).primaryColor,
+                        _passwordController,
+                        null,
+                        ValidatorType.password,
+                        true,
+                        _changePwError
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                        left: 20,
+                        right: 20,
+                        bottom: MediaQuery.of(context)
+                            .viewInsets.bottom
+                    ),
+                    child: Container(
                       height: 50,
-                      width: 50,
-                      child: Stack(
-                        children: <Widget>[
-                          Positioned(
-                            left: 18,
-                            top: 18,
-                            child: Container(
-                              width: 32,
-                              height: 32,
-                              decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: Theme.of(context).primaryColor
-                              ),
-                            ),
-                          ),
-                          Positioned(
-                            left: 8,
-                            top: 8,
-                            child: IconButton(
-                              onPressed: () {
-                                Navigator.of(context).pop();
-                                _passwordController.clear();
-                                _oldPasswordController.clear();
-                              },
-                              icon: Icon(
-                                  FontAwesomeIcons.angleLeft,
-                                  size: 30.0,
-                                  color: Colors.white
-                              ),
-                            ),
-                          )
-                        ],
+                      width: MediaQuery.of(context).size.width,
+                      child: filledButton(
+                          "SAVE",
+                          Colors.white,
+                          Theme.of(context).primaryColor,
+                          Theme.of(context).primaryColor,
+                          Colors.white,
+                          _validateAndChangePW
                       ),
                     ),
-                    SingleChildScrollView(
-                      child: Column(
-                        children: <Widget>[
-                          Padding(
-                            padding: EdgeInsets.only(top: 20, bottom: 8),
-                            child: Text(
-                              'Please first enter the old password',
-                              textAlign: TextAlign.center,
-                              overflow: TextOverflow.ellipsis,
-                              style: TextStyle(
-                                  fontSize: 16
-                              ),
-                            ),
-                          ),
-                          Form(
-                            key: _formKey,
-                            child: Column(
-                              children: <Widget>[
-                                Padding(
-                                  padding: EdgeInsets.only(bottom: 20),
-                                  child: inputAuthentication(
-                                      Icon(FontAwesomeIcons.unlockAlt),
-                                      "OLD PASSWORD",
-                                      Theme.of(context).primaryColor,
-                                      _oldPasswordController,
-                                      null,
-                                      ValidatorType.password,
-                                      true,
-                                      _reauthError
-                                  ),
-                                ),
-                                Padding(
-                                  padding: EdgeInsets.only(bottom: 20),
-                                  child: inputAuthentication(
-                                      Icon(FontAwesomeIcons.unlockAlt),
-                                      "NEW PASSWORD",
-                                      Theme.of(context).primaryColor,
-                                      _passwordController,
-                                      null,
-                                      ValidatorType.password,
-                                      true,
-                                      _changePwError
-                                  ),
-                                ),
-                                Padding(
-                                  padding: EdgeInsets.only(
-                                      left: 20,
-                                      right: 20,
-                                      bottom: MediaQuery.of(context)
-                                          .viewInsets.bottom
-                                  ),
-                                  child: Container(
-                                    height: 50,
-                                    width: MediaQuery.of(context).size.width,
-                                    child: filledButton(
-                                        "SAVE",
-                                        Colors.white,
-                                        Theme.of(context).primaryColor,
-                                        Theme.of(context).primaryColor,
-                                        Colors.white,
-                                        _validateAndChangePW
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          SizedBox(
-                            height: 20,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
-          ),
-        ],
+            SizedBox(
+              height: 20,
+            ),
+          ],
+        ),
       ),
     );
   }
