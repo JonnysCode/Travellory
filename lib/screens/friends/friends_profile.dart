5f9b76a8165a23cc85d2e389463037dd945e2b14
unknown <fluetfab@students.zhaw.ch>
2020-05-19 23:17:19 +0200

    add scrollable static achievements on friends-profile

--- lib/screens/friends/friends_profile.dart
+++ lib/screens/friends/friends_profile.dart
@@ -19,18 +19,11 @@ class FriendsProfile extends StatefulWidget {
 }
 
 class _FriendsProfileState extends State<FriendsProfile> {
-//  final List<String> achievements = <String>[
-//    'World                  20%',
-//    'Europe                50%',
-//    'Asia                       5%',
-//    'North America   10%',
-//    'South America   15%',
-//  ];
-
   @override
   Widget build(BuildContext context) {
     final FriendsModel friend = ModalRoute.of(context).settings.arguments;
-    Achievements achievementsProvider = Provider.of<AchievementsProvider>(context).achievements;
+    // TODO(bertaben): fetch achievements from friend
+//    Achievements achievementsProvider = Provider.of<AchievementsProvider>(context).achievements;
 //    List<String> achievements = achievementsProvider.achievements;
 //    List<int> percentages = achievementsProvider.percentages;
     final  List<String> achievements = <String>[
@@ -45,147 +38,113 @@ class _FriendsProfileState extends State<FriendsProfile> {
     ];
 
     final List<int> percentages = <int>[
-      0,1,2,3,4,5,6,7
+      3,10,1,5,0,2,0,0
     ];
 
-
+    // TODO(bertanben,fluetfab): create a widget out of the achievementsView
     return Scaffold(
       key: Key('friends_profile'),
       body: Container(
+        padding: EdgeInsets.only(bottom: 10),
         color: Colors.white,
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
             FriendsProfileHeader(friend: friend),
-            Padding(
-              key: Key('friends_trips'),
-              padding: EdgeInsets.only(
-                top: 30,
-                left: 20,
-                right: 90,
-                bottom: MediaQuery.of(context).viewInsets.bottom
-              ),
-              child: Container(
-                height: 30,
-                width: MediaQuery.of(context).size.width,
-                child: FashionFetishText(
-                  text: 'Trips' ,
-                  size: 22,
-                  fontWeight: FashionFontWeight.heavy,
-                ),
-              ),
-            ),
-            Container(
-              constraints: BoxConstraints(minHeight: 20, maxHeight: 300),
-              child: Padding(
-                key: Key('friends_trips_list'),
-                  padding: EdgeInsets.only(
-                      top: 5,
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(children: <Widget>[
+                  Padding(
+                    key: Key('friends_trips'),
+                    padding: EdgeInsets.only(
+                      top: 30,
                       left: 20,
+                      right: 90,
                       bottom: MediaQuery.of(context).viewInsets.bottom
+                    ),
+                    child: Container(
+                      height: 30,
+                      width: MediaQuery.of(context).size.width,
+                      child: FashionFetishText(
+                        text: 'Trips' ,
+                        size: 22,
+                        fontWeight: FashionFontWeight.heavy,
+                      ),
+                    ),
                   ),
-                  child: Text("Your friend hasn't created any trips yet.")
-              ),
-            ),
-            SizedBox(height: 20),
-            Padding(
-              key: Key('friends_achievements'),
-              padding: EdgeInsets.only(
-                  top: 30,
-                  left: 20,
-                  right: 90,
-                  bottom: MediaQuery.of(context).viewInsets.bottom
-              ),
-              child: Container(
-                height: 30,
-                width: MediaQuery.of(context).size.width,
-                child: FashionFetishText(
-                  text: 'Achievements' ,
-                  size: 22,
-                  fontWeight: FashionFontWeight.heavy,
-                ),
-              ),
-            ),
-            SingleChildScrollView(
-              child: Column(children: <Widget>[
-                Padding(padding: const EdgeInsets.only(top: 10, left: 15, right: 15)),
-                for (int i = 0; i < achievements.length; i++)
                   Container(
-                      key: Key(achievements[i]),
-                      height: 81,
-                      child: Stack(children: <Widget>[
-                        Padding(
-                          padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-                          child: Column(children: <Widget>[
-                            SizedBox(height: 20),
-                            FashionFetishText(
-                              text: achievements[i],
-                              size: 16,
-                              fontWeight: FashionFontWeight.heavy,
-                            ),
-                            SizedBox(height: 10),
-                            GFProgressBar(
-                              percentage: (percentages[i] / 100),
-                              backgroundColor: Colors.black26,
-                              progressBarColor: Theme.of(context).primaryColor,
-                              width: MediaQuery.of(context).size.width - 86,
-                              lineHeight: 40.0,
-                              child: Padding(
-                                padding: EdgeInsets.only(top: 7),
-                                child: Text(
-                                  '${percentages[i]}%',
-                                  textAlign: TextAlign.start,
-                                  style: TextStyle(fontSize: 22, color: Colors.white),
+                    constraints: BoxConstraints(minHeight: 20, maxHeight: 300),
+                    child: Padding(
+                      key: Key('friends_trips_list'),
+                      padding: EdgeInsets.only(
+                        top: 5,
+                        left: 20,
+                        bottom: MediaQuery.of(context).viewInsets.bottom
+                      ),
+                      child: Text("Your friend hasn't created any trips yet.")
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Padding(
+                    key: Key('friends_achievements'),
+                    padding: EdgeInsets.only(
+                      top: 30,
+                      left: 20,
+                      right: 90,
+                      bottom: MediaQuery.of(context).viewInsets.bottom
+                    ),
+                    child: Container(
+                      height: 30,
+                      width: MediaQuery.of(context).size.width,
+                      child: FashionFetishText(
+                        text: 'Achievements' ,
+                        size: 22,
+                        fontWeight: FashionFontWeight.heavy,
+                      ),
+                    ),
+                  ),
+                  Column(children: <Widget>[
+                    Padding(padding: const EdgeInsets.only(top: 10, left: 15, right: 15)),
+                    for (int i = 0; i < achievements.length; i++)
+                      Container(
+                        key: Key(achievements[i]),
+                        height: 81,
+                        child: Stack(children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.fromLTRB(30, 0, 20, 0),
+                            child: Column(children: <Widget>[
+                              SizedBox(height: 20),
+                              FashionFetishText(
+                                text: achievements[i],
+                                size: 16,
+                                fontWeight: FashionFontWeight.heavy,
+                              ),
+                              SizedBox(height: 10),
+                              GFProgressBar(
+                                percentage: (percentages[i] / 100),
+                                backgroundColor: Colors.black26,
+                                progressBarColor: Theme.of(context).primaryColor,
+                                width: MediaQuery.of(context).size.width - 86,
+                                lineHeight: 40.0,
+                                child: Padding(
+                                  padding: EdgeInsets.only(top: 7),
+                                  child: Text(
+                                    '${percentages[i]}%',
+                                    textAlign: TextAlign.start,
+                                    style: TextStyle(fontSize: 22, color: Colors.white),
+                                  ),
                                 ),
                               ),
-                            ),
-                          ]),
-                        )
-                      ])),
-              ])
+                            ]),
+                          )
+                        ])
+                      ),
+                  ]),
+                  Padding(padding: EdgeInsets.only(bottom: 50)),
+                ])
+              ),
             )
-
-//            Expanded(
-//              child: Padding(
-//                key: Key('friends_achievements_list'),
-//                padding: EdgeInsets.only(
-//                    left: 20,
-//                    right: 20,
-//                    bottom: MediaQuery.of(context).viewInsets.bottom),
-//                child: Scrollbar(
-//                  child: Visibility(
-//                    visible: true,
-//                    child: ListView.separated(
-//                      padding: EdgeInsets.only(
-//                        bottom: 30,
-//                      ),
-//                      scrollDirection: Axis.vertical,
-//                      shrinkWrap: true,
-//                      separatorBuilder: (context, index) =>
-//                      const SizedBox(height: 12),
-//                      itemCount: achievements.length,
-//                      itemBuilder: (context, index) {
-//                        return Text(
-//                          '${achievements[index]}',
-//                          style: TextStyle(
-//                            fontSize: 18
-//                          ),
-//                        );
-//                      },
-//                    ),
-//                    replacement: Padding(
-//                        key: Key('friends_achievements_list'),
-//                        padding: EdgeInsets.only(
-//                            top: 5,
-//                            bottom: MediaQuery.of(context).viewInsets.bottom
-//                        ),
-//                        child: Text("Your friend has no archievements so far.")
-//                    ),
-//                  ),
-//                ),
-//              ),
-//            ),
           ],
         ),
       ),
