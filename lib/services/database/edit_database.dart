763b70e22ac74da419080d97fc0304f6846e748a
antilyas <yasmine.antille@gmail.com>
2020-05-19 10:36:46 +0200

    + added calculate nights method for accommodation edit

--- lib/services/database/edit_database.dart
+++ lib/services/database/edit_database.dart
@@ -1,9 +1,11 @@
 import 'package:cloud_functions/cloud_functions.dart';
 import 'package:flutter/material.dart';
+import 'package:travellory/models/accommodation_model.dart';
 import 'package:travellory/providers/trips/trips_provider.dart';
 import 'package:travellory/utils/logger.dart';
 import 'package:travellory/models/abstract_model.dart';
 import 'package:travellory/providers/trips/single_trip_provider.dart';
+import 'package:travellory/widgets/forms/calculate_nights.dart';
 import 'package:travellory/widgets/forms/show_dialog.dart';
 import '../../widgets/bookings/edit.dart';
 
@@ -54,6 +56,10 @@ void Function() onEditBooking(
     SingleTripProvider singleTripProvider, Model model, BuildContext context, String functionName) {
 
   return () async {
+    if (model is AccommodationModel) {
+      model = calculateNightsForAccommodation(model);
+    }
+
     final bool edited = await singleTripProvider.editBooking(model, functionName);
     if (edited) {
       showEditedBookingDialog(context, onEditSuccessfulText);
