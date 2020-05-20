b292da590feacf9d196ce55b66f407199c0eba89
unknown <fluetfab@students.zhaw.ch>
2020-05-20 19:01:07 +0200

    create a widget for achievementsView

--- lib/screens/achievements/view_achievements.dart
+++ lib/screens/achievements/view_achievements.dart
@@ -5,6 +5,7 @@ import 'package:travellory/models/flight_model.dart';
 import 'package:travellory/models/user_model.dart';
 import 'package:travellory/providers/achievements_provider.dart';
 import 'package:travellory/services/database/delete_database.dart';
+import 'package:travellory/widgets/achievements_widget.dart';
 import 'package:travellory/widgets/bookings/view_booking_header.dart';
 import 'package:travellory/widgets/bookings/view_bookings.dart';
 import 'package:travellory/widgets/font_widgets.dart';
@@ -54,40 +55,11 @@ class _AchievementsViewState extends State<AchievementsView> {
       key: Key('AchievementsViewPage'),
       child: Column(children: [
         BookingHeader(headerTitle, bannerUrl),
-        Padding(padding: const EdgeInsets.only(top: 10, left: 15, right: 15)),
-        for (int i = 0; i < entries.length; i++)
-          Container(
-              key: Key(entries[i]),
-              height: 81,
-              child: Stack(children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-                  child: Column(children: <Widget>[
-                    SizedBox(height: 20),
-                    FashionFetishText(
-                      text: entries[i],
-                      size: 16,
-                      fontWeight: FashionFontWeight.heavy,
-                    ),
-                    SizedBox(height: 10),
-                    GFProgressBar(
-                      percentage: (percentages[i] / 100),
-                      backgroundColor: Colors.black26,
-                      progressBarColor: Theme.of(context).primaryColor,
-                      width: MediaQuery.of(context).size.width - 86,
-                      lineHeight: 40.0,
-                      child: Padding(
-                        padding: EdgeInsets.only(top: 7),
-                        child: Text(
-                          '${percentages[i]}%',
-                          textAlign: TextAlign.start,
-                          style: TextStyle(fontSize: 22, color: Colors.white),
-                        ),
-                      ),
-                    ),
-                  ]),
-                )
-              ])),
+        achievementsWidget(
+            context: context,
+            entries: entries,
+            percentages: percentages
+        ),
       ]),
     );
   }
