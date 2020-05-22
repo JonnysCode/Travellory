7bff627780c50a49b6d4c76cc8a12d1cedc2dbb3
antilyas <yasmine.antille@gmail.com>
2020-05-22 16:06:10 +0200

    * date time test changes

--- test/widget_test/forms/date_and_time_tests.dart
+++ test/widget_test/forms/date_and_time_tests.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:provider/provider.dart';
 import 'package:travellory/models/accommodation_model.dart';
 import 'package:travellory/models/rental_car_model.dart';
 import 'package:travellory/models/trip_model.dart';
+import 'package:travellory/providers/trips/trips_provider.dart';
 import 'package:travellory/widgets/forms/date_form_field.dart';
 import 'package:travellory/widgets/forms/time_form_field.dart';
+
+class TripsProviderMock extends Mock implements TripsProvider {}
 
 void main() {
   testWidgets('Test Date Field required exists and validation for empty fails',
       (WidgetTester tester) async {
     final GlobalKey<FormState> testKey = GlobalKey<FormState>();
+    TripsProviderMock tripsProvider = TripsProviderMock();
 
     // for testing purposes we will use a rental car model here
     final RentalCarModel testModel = RentalCarModel();
@@ -21,17 +27,20 @@ void main() {
       chosenDateString: (value) => testModel.pickupDate = value,
     );
 
-    Widget makeTestableWidget() {
-      return MaterialApp(
-        home: MediaQuery(
-          data: const MediaQueryData(devicePixelRatio: 1.0),
-          child: Directionality(
-            textDirection: TextDirection.ltr,
-            child: Center(
-              child: Material(
-                child: Form(
-                  key: testKey,
-                  child: testDate,
+    Widget makeTestableWidget(TripsProvider tripsProvider) {
+      return ChangeNotifierProvider<TripsProvider>.value(
+        value: tripsProvider,
+        child: MaterialApp(
+          home: MediaQuery(
+            data: const MediaQueryData(devicePixelRatio: 1.0),
+            child: Directionality(
+              textDirection: TextDirection.ltr,
+              child: Center(
+                child: Material(
+                  child: Form(
+                    key: testKey,
+                    child: testDate,
+                  ),
                 ),
               ),
             ),
@@ -41,7 +50,7 @@ void main() {
     }
 
     // Build our app and trigger a frame.
-    await tester.pumpWidget(makeTestableWidget());
+    await tester.pumpWidget(makeTestableWidget(tripsProvider));
 
     bool validateForm() {
       return (testKey.currentState.validate());
@@ -359,5 +368,77 @@ void main() {
     await enterDate('01-05-2020', testDateFieldOneKey, tester);
     await tester.pump();
     expect(find.text('01-05-2020', skipOffstage: false), findsOneWidget);
+  });
+
+  testWidgets('Test Date Field is in trip range', (WidgetTester tester) async {
+    TripsProviderMock tripsProvider = TripsProviderMock();
+
+    final GlobalKey<FormState> testKey = GlobalKey<FormState>();
+    final GlobalKey<DateFormFieldState> testDateFieldOneKey = GlobalKey<DateFormFieldState>();
+    final String dateInTripValidationMessage = 'The chosen date is not in trip date range';
+
+    final AccommodationModel testModel = AccommodationModel()
+      ..type = 'hotel'
+      ..name = 'Travelodge'
+      ..checkinDate = '01-05-2020'
+      ..address = "100 King's Cross Rd, London WC1X 9DT";
+
+    TripModel tripModel = TripModel(
+        name: 'Test Trip',
+        startDate: '01-05-2020',
+        endDate: '05-05-2020',
+        destination: 'London',
+        imageNr: 3);
+
+    final DateFormField testDateOne = DateFormField(
+      key: testDateFieldOneKey,
+      initialValue: testModel.checkoutDate,
+      labelText: 'Test Check Out Date',
+      icon: Icon(Icons.date_range),
+      optional: false,
+      tripModel: tripModel,
+      model: testModel,
+      chosenDateString: (value) => testModel.checkoutDate = value,
+    );
+
+    Widget makeTestableWidget(TripsProviderMock tripsProvider) {
+      return ChangeNotifierProvider<TripsProvider>.value(
+        value: tripsProvider,
+        child: MaterialApp(
+          home: MediaQuery(
+            data: const MediaQueryData(devicePixelRatio: 1.0),
+            child: Directionality(
+              textDirection: TextDirection.ltr,
+              child: Center(
+                child: Material(
+                  child: Form(
+                    key: testKey,
+                    child: testDateOne,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      );
+    }
+
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(makeTestableWidget(tripsProvider));
+
+    bool validateForm() {
+      return (testKey.currentState.validate());
+    }
+
+    Future<void> checkValidationFails() async {
+      await tester.tap(find.byType(TextFormField));
+      await enterDate('10-05-2020', testDateFieldOneKey, tester);
+      await tester.pump();
+      bool result = validateForm();
+      expect(result, isFalse);
+      expect(find.text(dateInTripValidationMessage, skipOffstage: false), findsOneWidget);
+    }
+
+    await checkValidationFails();
   });
 }
