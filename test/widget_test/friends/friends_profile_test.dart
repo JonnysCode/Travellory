718f220abe74bf4eb72dff73759d6af3e9ad1006
hessgia1 <hessgia1@students.zhaw.ch>
2020-05-23 05:28:54 +0200

    Added friends profile test

---
+++ test/widget_test/friends/friends_profile_test.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:travellory/models/achievements_model.dart';
+import 'package:travellory/models/friend_model.dart';
+import 'package:travellory/screens/friends/friends_profile.dart';
+
+void main() {
+  Future<void> pumpArgumentWidget(
+    WidgetTester tester, {
+    @required Object args,
+    @required Widget child,
+  }) async {
+    final key = GlobalKey<NavigatorState>();
+    await tester.pumpWidget(
+      MaterialApp(
+        navigatorKey: key,
+        home: FlatButton(
+          onPressed: () => key.currentState.push(
+            MaterialPageRoute<void>(
+              settings: RouteSettings(arguments: args),
+              builder: (_) => child,
+            ),
+          ),
+          child: const SizedBox(),
+        ),
+      ),
+    );
+  }
+
+  testWidgets('test profile page',
+      (WidgetTester tester) async {
+    FriendsProfile page = FriendsProfile();
+    FriendModel friend = FriendModel('uidFriend', 'friendName', null, '');
+
+    final List<Object> args = [];
+    args.add(friend);
+    args.add(Achievements());
+
+    await pumpArgumentWidget(tester, args: args, child: page);
+    await tester.tap(find.byType(FlatButton));
+    await tester.pumpAndSettle();
+
+    expect(find.byKey(Key('friends_profile')), findsOneWidget);
+  });
+}
