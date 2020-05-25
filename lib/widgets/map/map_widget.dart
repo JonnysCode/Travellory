a6875fd169d147a0107171fea48cbd819450555e
antilyas <yasmine.antille@gmail.com>
2020-05-25 17:59:02 +0200

    + added some documentation

--- lib/widgets/map/map_widget.dart
+++ lib/widgets/map/map_widget.dart
@@ -21,6 +21,7 @@ final log = getLogger('MapPage');
 final List<String> userStates = List<String>();
 String mapStyle;
 
+/// builds a user's personalised map page with individual trip infos and visited countries
 class GoogleMapWidget extends StatefulWidget {
   @override
   State<GoogleMapWidget> createState() => GoogleMapWidgetState();
@@ -32,61 +33,66 @@ class GoogleMapWidgetState extends State<GoogleMapWidget> {
   final Map<String, Marker> _markers = {};
   final List<Polygon> _boundaries = [];
 
+  /// fetches all markers for accommodation and activity locations belonging
+  /// to any of the user's trips
   Future<void> fetchMarkers(String activityType) async {
-    final List<SingleTripProvider> trips =
-        Provider.of<TripsProvider>(context, listen: false).trips;
+    final List<SingleTripProvider> trips = Provider.of<TripsProvider>(context, listen: false).trips;
 
     _markers.clear();
 
     for (final SingleTripProvider trip in trips) {
-      switch(activityType){
-        case "accommodations": {
-          final List<AccommodationModel> accommodations = trip.accommodations;
-          for (final AccommodationModel accommodation in accommodations) {
-            final marker = Marker(
-              markerId: MarkerId(accommodation.name),
-              position: LatLng(accommodation.latitude, accommodation.longitude),
-              infoWindow: InfoWindow(
-                  title: accommodation.name,
-                  snippet: accommodation.address,
-                  onTap: () {
-                    Navigator.pushNamed(context, AccommodationView.route, arguments: accommodation);
-                  }),
-            );
-            _markers[accommodation.tripUID] = marker;
+      switch (activityType) {
+        case "accommodations":
+          {
+            final List<AccommodationModel> accommodations = trip.accommodations;
+            for (final AccommodationModel accommodation in accommodations) {
+              final marker = Marker(
+                markerId: MarkerId(accommodation.name),
+                position: LatLng(accommodation.latitude, accommodation.longitude),
+                infoWindow: InfoWindow(
+                    title: accommodation.name,
+                    snippet: accommodation.address,
+                    onTap: () {
+                      Navigator.pushNamed(context, AccommodationView.route,
+                          arguments: accommodation);
+                    }),
+              );
+              _markers[accommodation.tripUID] = marker;
+            }
           }
-        }
-        break;
+          break;
 
-        case "activities": {
-          final List<ActivityModel> activities = trip.activities;
-          for (final ActivityModel activity in activities) {
-            final marker = Marker(
-              markerId: MarkerId(activity.title),
-              position: LatLng(activity.latitude, activity.longitude),
-              infoWindow: InfoWindow(
-                  title: activity.title,
-                  snippet: activity.location,
-                  onTap: () {
-                    Navigator.pushNamed(context, ActivityView.route, arguments: activity);
-                  }),
-            );
-            _markers[activity.tripUID] = marker;
+        case "activities":
+          {
+            final List<ActivityModel> activities = trip.activities;
+            for (final ActivityModel activity in activities) {
+              final marker = Marker(
+                markerId: MarkerId(activity.title),
+                position: LatLng(activity.latitude, activity.longitude),
+                infoWindow: InfoWindow(
+                    title: activity.title,
+                    snippet: activity.location,
+                    onTap: () {
+                      Navigator.pushNamed(context, ActivityView.route, arguments: activity);
+                    }),
+              );
+              _markers[activity.tripUID] = marker;
+            }
           }
-        }
-        break;
+          break;
 
-        default: {
-          return;
-        }
-        break;
+        default:
+          {
+            return;
+          }
+          break;
       }
     }
   }
 
   Future<void> refreshMarkers(String activityType) async {
     await fetchMarkers(activityType);
-    if (this.mounted){
+    if (this.mounted) {
       setState(() {});
     }
   }
@@ -96,9 +102,9 @@ class GoogleMapWidgetState extends State<GoogleMapWidget> {
     try {
       dynamic result = await UserManagement().getAchievements(_user.uid);
       dynamic _visitedCountries = result["visitedCountries"];
-      for(var countries in _visitedCountries){
+      for (var countries in _visitedCountries) {
         log.i(countries.toString());
-        if(!userStates.contains(countries.toString())){
+        if (!userStates.contains(countries.toString())) {
           userStates.add(countries.toString());
         }
       }
@@ -132,7 +138,7 @@ class GoogleMapWidgetState extends State<GoogleMapWidget> {
         ),
         gestureRecognizers: <Factory<OneSequenceGestureRecognizer>>[
           Factory<OneSequenceGestureRecognizer>(
-                () => EagerGestureRecognizer(),
+            () => EagerGestureRecognizer(),
           ),
         ].toSet(),
         onMapCreated: (GoogleMapController controller) {
@@ -160,16 +166,8 @@ class GoogleMapWidgetState extends State<GoogleMapWidget> {
           fabMargin: EdgeInsets.only(right: 20.0, bottom: 70),
           alignment: Alignment.bottomRight,
           children: <Widget>[
-            IconButton(
-                icon: Icon(FontAwesomeIcons.home),
-                color: Colors.white,
-                onPressed: () {
-                }),
-            IconButton(
-                icon: Icon(FontAwesomeIcons.plane),
-                color: Colors.white,
-                onPressed: () {
-                }),
+            IconButton(icon: Icon(FontAwesomeIcons.home), color: Colors.white, onPressed: () {}),
+            IconButton(icon: Icon(FontAwesomeIcons.plane), color: Colors.white, onPressed: () {}),
             IconButton(
                 icon: Icon(FontAwesomeIcons.theaterMasks),
                 color: Colors.white,
@@ -182,9 +180,7 @@ class GoogleMapWidgetState extends State<GoogleMapWidget> {
                 onPressed: () {
                   refreshMarkers("accommodations");
                 }),
-          ]
-      ),
-    ]
-    );
+          ]),
+    ]);
   }
 }
